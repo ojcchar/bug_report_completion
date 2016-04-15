4fe7bcf5b54a66fbe4f72be1390c4dae5691356d
fzampetti <fiorellazampetti@gmail.com>
2016-04-15 11:20:24 +0200

    Fix Labeled_List pattern

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/LabeledListPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/LabeledListPM.java
@@ -14,36 +14,56 @@ public class LabeledListPM extends StepsToReproducePatternMatcher {
 	public int matchSentence(Sentence sentence) throws Exception {
 		return 0;
 	}
-
+	
 	@Override
 	public int matchParagraph(Paragraph paragraph) throws Exception {
 
 		List<Sentence> sentences = paragraph.getSentences();
-
+		String text="";
+		int stepNumber=0;
+		int verbSentenceNumber=0;
 		if (sentences.size() > 1) {
-			Sentence sentence = sentences.get(0);
-			String text = TextProcessor.getStringFromLemmas(sentence);
-			if (text.matches("reproducible.*")) {
-				sentence = sentences.get(1);
+			//verify if there is a numbered(bullet) sequence in the paragraph
+			for (Sentence sentence : sentences) {
+				List<Token>tokens=sentence.getTokens();
 				text = TextProcessor.getStringFromLemmas(sentence);
-			}
-			// boolean b =
-			// ((text.matches(".*step.*"))||(text.matches(".*to reproduce.*"))||(text.matches("str :"))||(text.matches(".*try :")));
-			boolean b = false;
-			b = (text.matches(".*(following|repro) step.*"))
-					|| (text.matches("(step)? ?to repro.*"))
-					|| (text.equals("step :")) || (text.equals("str :"))
-					|| (text.endsWith("have try :"));
-			if (b) {
-				for (Sentence sen : sentences) {
-					text = TextProcessor.getStringFromLemmas(sen);
-					if (text.matches("^(\\d+|\\-).*")) {
-						return 1;
-					}
+				if (text.matches("^(\\d+|\\-).+")) {
+					stepNumber++;
+				}else if(tokens.size()>1 && tokens.get(0).getGeneralPos().equals("VB")){
+					verbSentenceNumber++;
 				}
 			}
+			if (verbSentenceNumber>0 || stepNumber>0){
+				//the paragraph will start with a sentence likes steps to reproduce
+				Sentence sentence = sentences.get(0);
+				text=TextProcessor.getStringFromLemmas(sentence);
+				if (text.matches("reproducible.*")) {
+					sentence = sentences.get(1);
+					text = TextProcessor.getStringFromLemmas(sentence);
+				}
+				//case like Repro steps:, reported in the following steps, steps to reproduce, what i have tried:
+				boolean b  = (text.matches(".*(following|repro) step.*"))
+						|| (text.matches(".*?(step)? ?to repro.*"))
+						|| (text.equals("step :")) || (text.equals("str :"))
+						|| (text.endsWith("have try :")) || (text.contains("here be the step"));
+				if(b==false){
+					sentence=sentences.get(1);
+					b=(text.matches(".*(following|repro) step.*"))
+							|| (text.matches(".*?(step)? ?to repro.*"))
+							|| (text.equals("step :")) || (text.equals("str :"))
+							|| (text.endsWith("have try :"))|| (text.contains("here be the step"));
+				}
+				if(b){
+					return 1;
+				}
+				
+			}
+			if(stepNumber>0){
+				return 1;
+			}
 		}
+		
 		return 0;
 	}
-
 }
+
