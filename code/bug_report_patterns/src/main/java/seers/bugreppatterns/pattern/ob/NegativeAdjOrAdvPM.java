c772153cad18c21e46a4bbee1f7e47d439cd6668
fzampetti <fiorellazampetti@gmail.com>
2016-04-17 18:42:36 +0200

    S_OB_NEG_ADJ_IN implemented

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeAdjOrAdvPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeAdjOrAdvPM.java
@@ -18,7 +18,7 @@ public class NegativeAdjOrAdvPM extends ObservedBehaviorPatternMatcher {
 			"empty", "off", "undefined", "slow", "untrusted", "bad", "corrupt", "wrong", "strange", "blank",
 			"undefined", "bloken", "infinite", "unstyled", "meaningless", "spurious", "inappropriate", "broken",
 			"weird", "down", "dirty", "expensive", "inaccessible", "unexpected", "different", "duplicated", "unlimited",
-			"inconsitant", "multiple", "unusable" };
+			"inconsitant", "multiple", "unusable", "duplicate" , "misplace"};
 
 	final public static String[] NEGATIVE_ADV = { "incorrectly", "randomly", "suddenly", "erroneously", "slow",
 			"indefinitely", "twice" };
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/NegativeAdjInPMTest.java
@@ -0,0 +1,11 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class NegativeAdjInPMTest extends BaseTest{
+	
+	public NegativeAdjInPMTest(){
+		pm = new NegativeAdjInPM();
+	}
+
+}
