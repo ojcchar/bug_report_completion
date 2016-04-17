d832e9aa3187fcfe4af83a70f05f77b937de65d4
fzampetti <fiorellazampetti@gmail.com>
2016-04-17 17:30:23 +0200

    S_OB_NEG_CONDITIONAL implemented, changed one pattern's assignment in all coded data

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeConditionalPM.java
@@ -0,0 +1,45 @@
+package seers.bugreppatterns.pattern.ob;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.bugreppatterns.pattern.PatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class NegativeConditionalPM extends ObservedBehaviorPatternMatcher{
+
+	final private static String [] TOKENS = {"when","while","if","whenever"};
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		ArrayList<Integer> indexes = foundIndexToken(tokens);
+		if(indexes.size()>0){
+			for (Integer integer : indexes) {
+				if(integer>0 && (integer<tokens.size()-1)){
+					Sentence negclause = new Sentence(sentence.getId(), tokens.subList(0, integer));
+					for (PatternMatcher pm : ButNegativePM.NEGATIVE_PMS) {
+						int match = pm.matchSentence(negclause);
+						if (match == 1) {
+							return 1;
+						}
+					}
+				}
+			}
+		}
+		return 0;
+	}
+	
+	private ArrayList<Integer> foundIndexToken(List<Token> tokens){
+		ArrayList<Integer> indexConditionalTerms = new ArrayList<Integer>();
+		for(int i=0;i<tokens.size();i++){
+			Token token = tokens.get(i);
+			if (Arrays.stream(TOKENS).anyMatch(t -> token.getLemma().contains(t))) {
+				indexConditionalTerms.add(i);
+			}
+		}
+		return indexConditionalTerms;
+	}
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/NegativeConditionalPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class NegativeConditionalPMTest extends BaseTest{
+
+	public NegativeConditionalPMTest(){
+		pm = new NegativeConditionalPM();
+	}
+}
