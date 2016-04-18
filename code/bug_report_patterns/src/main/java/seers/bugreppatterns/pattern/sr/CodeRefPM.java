066ed2556c91df7d7098075e5fca3202fdb5e310
fzampetti <fiorellazampetti@gmail.com>
2016-04-18 18:58:15 +0200

    Fix S_SR_CODE_REF as discussed with Oscar

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/CodeRefPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/CodeRefPM.java
@@ -10,52 +10,42 @@ import seers.textanalyzer.entity.Token;
 
 public class CodeRefPM extends StepsToReproducePatternMatcher{
 
-	final static String[] NOUNS_TERM = {"snippet","code","sample","configuration","statement","script","html/ssi","html/fbml","html","trace",".htm"};
+	final static String[] NOUNS_TERM = {"snippet","code","sample","configuration","statement","script","html/ssi","html/fbml","html","trace","test.htm"};
 	final static String[] ADV_LOCATION = {"here","below"};
 	final static String[] VERB_DEMO={"provide","enclose","follow","render","attach"};
 	
 	public int matchSentence(Sentence sentence) throws Exception {
 		
 		String text = TextProcessor.getStringFromLemmas(sentence);
-		
 		List<Token> tokens=sentence.getTokens();
-		
+		boolean isNounTerm = false;
+		boolean isAdverb = false;
+		boolean isVerbAsAdjective=false;
+		boolean endsWith=false;
+		if(text.endsWith(":")||text.matches(".*from :.*")||text.endsWith("e.g.")){endsWith=true;}
+		if(text.matches(".*(command line|live example|test case|file in).*")){
+			isNounTerm=true;
+		}
 		for(int i=0;i<tokens.size();i++){
 			Token token=tokens.get(i);
 			//exist a noun that refer to code such as code or snippet
-			if (Arrays.stream(NOUNS_TERM).anyMatch(t -> token.getLemma().contains(t))){
-				if(token.getGeneralPos().equalsIgnoreCase("NN")){
-					return 1;
+			if (Arrays.stream(NOUNS_TERM).anyMatch(t -> token.getLemma().equals(t))){
+				if(token.getGeneralPos().equals("NN")){
+					isNounTerm=true;
 				}
+			}else if(Arrays.stream(ADV_LOCATION).anyMatch(t -> token.getLemma().equals(t))&&((token.getGeneralPos().equals("RB"))||token.getGeneralPos().equals("IN"))){
+				isAdverb=true;
+			}else if(Arrays.stream(VERB_DEMO).anyMatch(t -> token.getLemma().equals(t))&&(token.getGeneralPos().equals("VB"))){
+				isVerbAsAdjective = true;
 			}
+
 		}
-		if(text.matches(".*(command line|live example|test case|file in).*")){
+
+		if(isNounTerm && (isAdverb||isVerbAsAdjective||endsWith)){
 			return 1;
-		}else{
-			//return existLocation(tokens)?1:0;
-			return 0;
 		}
+		return 0;
 		
 	}
 
-	/*private boolean existLocation(List<Token> tokens) {
-		boolean adverb=false;
-		for(int i=0;i<tokens.size();i++){
-			Token token = tokens.get(i);
-			if (Arrays.stream(ADV_LOCATION).anyMatch(t -> token.getLemma().equals(t))){
-				adverb=true;
-			}
-		}
-		if(adverb){
-			for(int i=0;i<tokens.size();i++){
-				Token token=tokens.get(i);
-				if(Arrays.stream(VERB_DEMO).anyMatch(t -> token.getLemma().equals(t))){
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-*/
 }
