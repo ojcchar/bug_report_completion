2b48ce6b10d12040974abc2033f3bec06cd1e1ec
fzampetti <fiorellazampetti@gmail.com>
2016-04-18 10:10:03 +0200

    S_OB_GERUND_SUBJECT implemented

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/GerundSubjectPM.java
@@ -0,0 +1,30 @@
+package seers.bugreppatterns.pattern.ob;
+
+import java.util.Arrays;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class GerundSubjectPM extends ObservedBehaviorPatternMatcher{
+
+	final private static String[] VERBS_AS_NOUNS = {"copy","drag","insert"};
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		Token first = tokens.get(0);
+		if(first.getPos().equals("VBG")||(first.getLemma().endsWith("ing")&& first.getGeneralPos().equals("NN"))){
+			for (int i=1;i<tokens.size();i++){
+				Token token = tokens.get(i);
+				if(token.getGeneralPos().equals("VB")){
+					return 1;
+				}else if(token.getGeneralPos().equals("NN")&&Arrays.stream(VERBS_AS_NOUNS).anyMatch(t -> token.getLemma().contains(t))){
+					return 1;
+				}
+			}
+		}
+		return 0;
+	}
+
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/GerundSubjectPMTest.java
@@ -0,0 +1,11 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class GerundSubjectPMTest extends BaseTest{
+	
+	public GerundSubjectPMTest(){
+		pm = new GerundSubjectPM();
+	}
+
+}
