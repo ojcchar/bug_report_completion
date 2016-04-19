94e868dfc88e55893a38bab932caf7fda5dcfe39
juan <jmflorezff@gmail.com>
2016-04-19 10:16:44 -0500

    Implemented and tested S_EB_WANT.

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/WantPMTest.java
@@ -0,0 +1,12 @@
+package seers.bugreppatterns.pattern.eb;
+
+import seers.bugreppatterns.main.BaseTest;
+
+/**
+ * Created by juan on 4/19/16.
+ */
+public class WantPMTest extends BaseTest {
+    public WantPMTest() {
+        pm = new WantPM();
+    }
+}
