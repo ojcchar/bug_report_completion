990aa2196bd4b2a474a30cfb4ccd967305d77e45
fzampetti <fiorellazampetti@gmail.com>
2016-04-19 12:10:16 +0200

    S_EB_COULD implemented

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/CouldPM.java
@@ -0,0 +1,30 @@
+package seers.bugreppatterns.pattern.eb;
+
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ExpectedBehaviorPatternMatcher;
+import seers.bugreppatterns.pattern.PatternMatcher;
+import seers.textanalyzer.TextProcessor;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class CouldPM extends ExpectedBehaviorPatternMatcher{
+
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		for(int i=0;i<tokens.size();i++){
+			if(tokens.get(i).getGeneralPos().equals("MD")&&(tokens.get(i).getLemma().equals("could"))){
+				PatternMatcher pm = new CouldQuestionSentencePM();
+				int match = pm.matchSentence(sentence);
+				if(match==0){
+					return 1;
+				}
+			}
+		}
+
+		
+		return 0;
+	}
+
+}
