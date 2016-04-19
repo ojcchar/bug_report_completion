022ceba80546006ae1360d58a0bbb5653db2eae5
fzampetti <fiorellazampetti@gmail.com>
2016-04-19 11:51:48 +0200

    S_EB_NORMALLY implemented

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/NormallyPM.java
@@ -0,0 +1,31 @@
+package seers.bugreppatterns.pattern.eb;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ExpectedBehaviorPatternMatcher;
+import seers.textanalyzer.TextProcessor;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class NormallyPM extends ExpectedBehaviorPatternMatcher {
+
+	final public static String[] ADVERBS = { "ideally", "perfectly",
+			"normally", "preferably", "typically", "generally", "hopefully",
+			"ordinarily", "popularly", "regularly", "usually", "widely",
+			"habitually", "frequently", "commonly", "traditionally" };
+
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		if(tokens.size()>1){
+			Token firstToken = tokens.get(0);
+			if(firstToken.getGeneralPos().equals("RB")&& Arrays.stream(ADVERBS).anyMatch(t->firstToken.getLemma().equals(t))){
+				return 1;
+			}
+		}
+		return 0;
+	}
+
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/NormallyPMTest.java
@@ -0,0 +1,11 @@
+package seers.bugreppatterns.pattern.eb;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class NormallyPMTest extends BaseTest{
+	
+	public NormallyPMTest(){
+		pm=new NormallyPM();
+	}
+
+}
