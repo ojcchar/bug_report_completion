e22cbda86b2c52770c4c13a540a4afefddeff4b5
fzampetti <fiorellazampetti@gmail.com>
2016-04-19 17:35:26 +0200

    S_SR_SIMPLE_PRESENT_SUBORDINATES implemented

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/SimplePresentSubordinatesPMTest.java
@@ -0,0 +1,11 @@
+package seers.bugreppatterns.pattern.sr;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class SimplePresentSubordinatesPMTest extends BaseTest{
+	
+	public SimplePresentSubordinatesPMTest(){
+		pm = new SimplePresentSubordinatesPM();
+	}
+
+}
