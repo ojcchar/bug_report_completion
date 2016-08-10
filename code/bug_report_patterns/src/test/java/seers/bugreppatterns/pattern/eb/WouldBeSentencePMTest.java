a54988d7ca21f589bc5cecb9f67362a2a1d467d3
Oscar Chaparro <ojcch1@gmail.com>
2016-08-10 08:37:39 -0500

    Code data updated + Eb pattern names

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/WouldBeSentencePMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/WouldBeSentencePMTest.java
@@ -1,11 +1,22 @@
 package seers.bugreppatterns.pattern.eb;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 public class WouldBeSentencePMTest extends BaseTest {
 
 	public WouldBeSentencePMTest() {
 		pm = new WouldBeSentencePM();
+	}
+
+	@Test
+	public void testPositives() throws Exception {
+
+		String[] txts = { "The error message would be more helpful if it said:" };
+		TestUtils.testSentences(txts, pm, 1);
+
 	}
 
 }
