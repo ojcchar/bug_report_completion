b7e039393a3bb8c6dfc4ccd744313bb20186127d
fzampetti <fiorellazampetti@gmail.com>
2016-08-12 15:34:56 +0200

    Implement and test S_OB_NEG_AFTER

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/NegativeAfterPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class NegativeAfterPMTest extends BaseTest{
+
+	public NegativeAfterPMTest() {
+		pm = new NegativeAfterPM();
+	}
+}
