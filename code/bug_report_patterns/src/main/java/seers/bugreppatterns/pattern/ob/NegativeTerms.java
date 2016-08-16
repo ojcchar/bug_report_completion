0178c11271fd3f700f64ad56185e008cedf9f5b6
Laura Moreno <lvmorenoc@gmail.com>
2016-08-16 10:39:18 -0600

    updated negative terms

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeTerms.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeTerms.java
@@ -1,18 +1,19 @@
 package seers.bugreppatterns.pattern.ob;
 
 public class NegativeTerms {
-	final public static String[] ADJECTIVES = { "abnormal", "agressive", "ambiguous", "annoying", "bad", "blank", "blocked",
-			"bloken", "broken", "counter-intuitive", "corrupt", "cpu-bound", "dead", "different", "dirty", "down",
-			"duplicated", "empty", "enormous", "erroneous", "expensive", "extra", "faulty", "funny-looking", "gone", "hard",
-			"ignored", "inaccessible", "inappropriate", "incorrect", "inconsitant", "inconsistent", "incredible",
-			"indefinite", "inefficient", "infinite", "insecure", "invalid", "lost", "meaningless", "messed up", "out-of date",
-			"misleading", "misplaced", "missing", "mistaken", "multiple", "off", "off-center", "old", "orphaned",
-			"out-of-date", "outdated", "painful", "poor", "random", "reduced", "redundant", "repeated", "reversed",
-			"runaway", "semi", "silly", "slow", "spurious", "squashed", "strange", "stripped", "stuck", "tedious", "terrible", "truncated",
-			"twice", "ugly", "unacceptable", "unchecked", "unclickable", "undefined", "undesirable", "undesired", "unexpected",
-			"unfriendly", "uninitialized", "unknown", "unlimited", "unreadable", "unreliable", "unresponsive", "unsecure", "unstable",
-			"unstoppable", "unstyled", "untrusted", "userunfriendly", "unusable", "voided", "wacky", "weird", "worst", "wrong",
-			"zombie" };
+	final public static String[] ADJECTIVES = { "abnormal", "agressive", "ambiguous", "annoying", "bad", "blank",
+			"blocked", "bloken", "broken", "confusing", "counter-intuitive", "corrupt", "cpu-bound", "dead",
+			"different", "dirty", "down", "duplicated", "empty", "enormous", "erroneous", "expensive", "extra",
+			"faulty", "frustrating", "funny-looking", "garbled", "gone", "hard", "ignored", "inaccessible",
+			"inappropriate", "incorrect", "inconsitant", "inconsistent", "incredible", "indefinite", "inefficient",
+			"infinite", "insecure", "invalid", "lost", "meaningless", "messed up", "out-of date", "misleading",
+			"misplaced", "missing", "mistaken", "multiple", "off", "off-center", "old", "orphaned", "out-of-date",
+			"outdated", "painful", "poor", "random", "reduced", "redundant", "repeated", "reversed", "runaway", "semi",
+			"silly", "slow", "spurious", "squashed", "strange", "stripped", "stuck", "tedious", "terrible", "truncated",
+			"twice", "ugly", "unacceptable", "unchecked", "unclickable", "undefined", "undesirable", "undesired",
+			"unexpected", "unfriendly", "uninitialized", "unknown", "unlimited", "unnavigability", "unnecessary",
+			"unreadable", "unreliable", "unresponsive", "unsecure", "unstable", "unstoppable", "unstyled", "untrusted",
+			"userunfriendly", "unusable", "voided", "wacky", "weird", "worst", "wrong", "zombie" };
 
 	final public static String[] ADVERBS = { "abnormally", "agressively", "annoyingly", "badly", "blankly", "corruptly",
 			"counter-intuitively", "deadly", "differently", "dirtly", "emptyly", "enourmously", "erroneously",
@@ -31,8 +32,10 @@ public class NegativeTerms {
 			"stop", "strip", "stuck up", "suffer", "terminate", "throw", "time out", "trim", "truncate", "vanish",
 			"wipe" };
 
-	final public static String[] NOUNS = { "error", "warning", "bug", "violation", "exception", "problem", "npe", "ise",
-			"fault", "404", "issue", "typo", "crash", "glitch", "failure", "errore", "truncation", "loss", "collision",
-			"leak", "conflict", "unnavigability", "lock", "zombie", "violation", "confusion", "duplicate", "peek",
-			"segfault", "nil pointer", "corruption", "duplicate", "truncate", "incorrect", "delay", "stacktrace" };
+	final public static String[] NOUNS = { "404", "ambiguity", "breakage", "bug", "collision", "conflict", "confusion",
+			"corruption", "crash", "cut", "delay", "disaster", "duplicate", "error", "errore", "exception", "failure",
+			"fault", "frustration", "glitch", "inability", "issue", "leak", "leakage", "lock", "loss", "mistake", "nil",
+			"NPE", "null", "omission", "pain", "peek", "problem", "race", "rarity", "runaway", "segfault",
+			"segmentation", "spam", "stacktrace", "status", "symptom", "truncation", "typo", "violation", "wait",
+			"warning", "zombie" };
 }
