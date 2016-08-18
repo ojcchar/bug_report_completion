6940fe4a7915cef605eadf614d8c71b56446dba0
Oscar Chaparro <ojcch1@gmail.com>
2016-08-17 20:16:15 -0500

    S_EB_INSTEAD_OF_EXP_BEHAVIOR checked and tested

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/InsteadExpBehaviorPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/InsteadExpBehaviorPM.java
@@ -8,6 +8,7 @@ public class InsteadExpBehaviorPM extends ExpectedBehaviorPatternMatcher {
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
+
 		String txt = TextProcessor.getStringFromLemmas(sentence);
 		if (txt.matches(".+(instead of|rather than|in lieu of).+")) {
 			return 1;
