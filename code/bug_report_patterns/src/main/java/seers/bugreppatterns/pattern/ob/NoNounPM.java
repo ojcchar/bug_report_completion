9794e0230a0a9447f43ab295b3c7b2222a434de0
Laura Moreno <lvmorenoc@gmail.com>
2016-08-17 20:12:00 -0600

    Revised S_OB_NO_NOUN pattern

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NoNounPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NoNounPM.java
@@ -1,53 +1,58 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.Arrays;
 import java.util.List;
 
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
-import seers.textanalyzer.TextProcessor;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
 public class NoNounPM extends ObservedBehaviorPatternMatcher {
 
+	private final static String[] NO_TERMS = { "no", "nothing", "none", "neither" };
+
+	private final static String[] POST_NO_TERMS = { "NN", "VB", "DT", "MD", "IN", "JJ", "RP", "WH" };
+
+	private final static String[] PRE_NO_POS = { "VB" };
+
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-		String text = TextProcessor.getStringFromLemmas(sentence);
 		List<Token> tokens = sentence.getTokens();
-		if (tokens.size() > 1) {
-			if (text.startsWith("no ") || (text.startsWith("nothing"))) {
-				if (tokens.get(1).getGeneralPos().equals("NN") || (tokens.get(1).getGeneralPos().equals("VB"))) {
+
+		if (!tokens.isEmpty()) {
+			int start = 0;
+			while (start < tokens.size()) {
+				Token current = tokens.get(start);
+				String previousGenPos = start == 0 ? "OK" : tokens.get(start - 1).getGeneralPos();
+				// if there is a verb before the NO_TERM, the pattern is S_OB_VERB_NO
+				if (Arrays.stream(NO_TERMS).anyMatch(t -> current.getLemma().equals(t))
+						&& !Arrays.stream(PRE_NO_POS).anyMatch(t -> previousGenPos.equals(t))) {
+					return matchSubSentence(new Sentence(sentence.getId(), tokens.subList(start, tokens.size())));
+				}
+				start++;
+			}
+		}
+
+		return 0;
+
+	}
+
+	public int matchSubSentence(Sentence sentence) throws Exception {
+		System.out.println(sentence);
+		List<Token> tokens = sentence.getTokens();
+		if (tokens.size() > 2) {
+			if (Arrays.stream(NO_TERMS).anyMatch(t -> tokens.get(0).getLemma().equals(t))) {
+				Token nextToken = tokens.get(1);
+				// Check the sentence is not S_OB_NO_LONGER
+				if (nextToken.getWord().equalsIgnoreCase("longer")) {
+					return 0;
+				} else if (Arrays.stream(POST_NO_TERMS).anyMatch(t -> nextToken.getGeneralPos().equals(t))) {
 					return 1;
 				}
-			} else if (text.startsWith("issue") || (text.startsWith("problem") || (text.startsWith("error")))) {
-				int match = isInner(tokens);
-				if (match == 1) {
-					return 1;
-				}
-			} else {
-				int match = isInner(tokens);
-				return match;
 			}
 		}
 
 		return 0;
 	}
 
-	public int isInner(List<Token> tokens) {
-		int index = 0;
-		for (Token token : tokens) {
-			if (index < tokens.size() - 2) {
-				if (token.getLemma().equals("no") || token.getLemma().equals("nothing")) {
-					if (tokens.get(index + 1).getGeneralPos().equals("NN")
-							|| (tokens.get(index + 1).getGeneralPos().equals("VB"))
-							|| (tokens.get(index + 1).getGeneralPos().equals("RP"))) {
-						return 1;
-					}
-				} else if (token.getLemma().equals("and") && (tokens.get(index + 1).getLemma().equals("not"))) {
-					return 1;
-				}
-			}
-			index++;
-		}
-		return 0;
-	}
 }
