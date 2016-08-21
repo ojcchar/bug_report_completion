88f12c5f2ad5493b3c5aeab43f443171ef4a9005
juan <jmflorezff@gmail.com>
2016-08-21 17:48:17 -0500

    Eliminated some false positives.

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeVerbPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeVerbPM.java
@@ -10,28 +10,28 @@ import seers.textanalyzer.entity.Token;
 
 public class NegativeVerbPM extends ObservedBehaviorPatternMatcher {
 
-	final private static String[] OTHER_NEGATIVE_VERBS = { "slow doen", "slow down", "faile", "stucks up",
-			"consume 100", "get turn into", "be out of", "pull out", "faul", "hangs/get", "failes", "timing out",
-			"go away" };
+    final private static String[] OTHER_NEGATIVE_VERBS = {"slow doen", "slow down", "faile",
+            "stucks up", "consume 100", "get turn into", "be out of", "pull out", " faul ",
+            "hangs/get", "failes", "timing out", "go away"};
 
-	@Override
-	public int matchSentence(Sentence sentence) throws Exception {
+    @Override
+    public int matchSentence(Sentence sentence) throws Exception {
 
-		List<Token> tokens = sentence.getTokens();
-		for (Token token : tokens) {
-			if (Arrays.stream(NegativeTerms.VERBS).anyMatch(t -> token.getLemma().startsWith(t))
-					&& (token.getGeneralPos().equals("VB") || token.getGeneralPos().equals("NN")
-							|| token.getGeneralPos().equals("JJ"))) {
-				return 1;
-			}
-		}
-		String txt = TextProcessor.getStringFromLemmas(sentence);
+        List<Token> tokens = sentence.getTokens();
+        for (Token token : tokens) {
+            if (Arrays.stream(NegativeTerms.VERBS).anyMatch(t -> token.getLemma().equals(t))
+                    && (token.getGeneralPos().equals("VB") || token.getGeneralPos().equals("NN")
+                    || token.getGeneralPos().equals("JJ"))) {
+                return 1;
+            }
+        }
+        String txt = TextProcessor.getStringFromLemmas(sentence);
 
-		if (Arrays.stream(OTHER_NEGATIVE_VERBS).anyMatch(p -> txt.contains(p))) {
-			return 1;
-		}
+        if (Arrays.stream(OTHER_NEGATIVE_VERBS).anyMatch(txt::contains)) {
+            return 1;
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 
 }
