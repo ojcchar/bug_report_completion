d913f5b6330074db9d2a65020420115c89dff7c2
Oscar Chaparro <ojcch1@gmail.com>
2016-08-22 17:02:03 -0500

    S_EB_BEFORE implemented and tested

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/BeforePastClausePM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/eb/BeforePastClausePM.java
@@ -1,60 +1,107 @@
 package seers.bugreppatterns.pattern.eb;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import seers.bugreppatterns.pattern.ExpectedBehaviorPatternMatcher;
+import seers.textanalyzer.TextProcessor;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
 public class BeforePastClausePM extends ExpectedBehaviorPatternMatcher {
 
+	private static final String[] BEFORE_EXPRESSIONS = { "before that", "previously", "in the past", "prior to",
+			"before" };
+
+	private static final int TOKENS_THRESHOLD;
+	static {
+		int maxExprLength = 0;
+		for (String expr : BEFORE_EXPRESSIONS) {
+			int length = expr.split(" ").length;
+			if (maxExprLength < length) {
+				maxExprLength = length;
+			}
+		}
+		TOKENS_THRESHOLD = maxExprLength + 3;
+	}
+
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 
 		List<Token> tokens = sentence.getTokens();
-		if (tokens.size() > 2) {
+		Integer idx = getFirstComma(tokens);
 
-			Token token1 = tokens.get(0);
-			if (token1.getLemma().equals("before")) {
-				Token token2 = tokens.get(1);
-				if (token2.getLemma().equals(",") || token2.getLemma().equals("_")) {
-					return checkPastClause(tokens.subList(2, tokens.size()));
-				} else {
-					if (token2.getGeneralPos().equals("DT")) {
-						Token token3 = tokens.get(2);
-						if (token3.getLemma().equals(",") || token3.getLemma().equals("_")) {
-							return checkPastClause(tokens.subList(3, tokens.size()));
-						}
-					}
-				}
+		if (idx == null) {
+			return 0;
+		}
 
-			}
+		if (matchesBeforeExpressions(tokens.subList(0, idx))) {
+			return checkPastClause(tokens.subList(idx + 1, tokens.size()));
 		}
 
 		return 0;
+	}
+
+	private boolean matchesBeforeExpressions(List<Token> tokens) {
+
+		String txt = TextProcessor.getStringFromLemmas(new Sentence("0", tokens));
+
+		return Arrays.stream(BEFORE_EXPRESSIONS).anyMatch(exp -> txt.contains(exp));
+	}
+
+	private Integer getFirstComma(List<Token> tokens) {
+		for (int i = 0; i < tokens.size() && i < TOKENS_THRESHOLD; i++) {
+			Token token = tokens.get(i);
+			if (token.getLemma().equals(",") || token.getLemma().equals("_")) {
+				return i;
+			}
+		}
+		return null;
 	}
 
 	private int checkPastClause(List<Token> tokens) {
 		List<Integer> verbs = findMainTokens(tokens);
 		for (Integer verb : verbs) {
-			if (verb - 1 >= 0) {
-				Token token = tokens.get(verb - 1);
-				if (token.getGeneralPos().equals("PRP") || token.getGeneralPos().equals("DT")
-						|| token.getGeneralPos().equals("NN")) {
+			Token verbToken = tokens.get(verb);
+
+			// no previous token
+			if (verb - 1 < 0) {
+				continue;
+			}
+			Token previousToken = tokens.get(verb - 1);
+
+			// case1: simple clauses in past tense, eg.: "I solved"
+			if (verbToken.getPos().equals("VBD")) {
+				if (previousToken.getGeneralPos().equals("PRP") || previousToken.getGeneralPos().equals("DT")
+						|| previousToken.getGeneralPos().equals("NN")) {
+					return 1;
+				}
+
+				// case2: "was/were solved"
+			} else if (verbToken.getPos().equals("VBN")) {
+				if (previousToken.getLemma().equals("be")) {
+					return 1;
+				}
+
+				// case 3: "would solve"
+			} else if (verbToken.getPos().equals("VB")) {
+				if (previousToken.getLemma().equals("would")) {
 					return 1;
 				}
 			}
 		}
 		return 0;
+
 	}
 
+	// all verbs
 	private List<Integer> findMainTokens(List<Token> tokens) {
 
 		List<Integer> mainToks = new ArrayList<>();
 		for (int i = 0; i < tokens.size(); i++) {
 			Token token = tokens.get(i);
-			if (token.getPos().equals("VBD")) {
+			if (token.getGeneralPos().equals("VB")) {
 				mainToks.add(i);
 			}
 		}
--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/BeforePastClausePMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/BeforePastClausePMTest.java
@@ -15,10 +15,20 @@ public class BeforePastClausePMTest extends BaseTest {
 	public void testPositives() throws Exception {
 
 		String[] txts = {
-				"Before that_ I was able to display the login and like buttons on the test page I set up for Facebook integration on our news site."
-
-		};
+				"Before that_ I was able to display the login and like buttons on the test page I set up for Facebook integration on our news site.",
+				"\"Previously, the comma operator (,) and JOIN both had the same precedence, so the join expression t1, t2 JOIN t3 was interpreted as ((t1, t2) JOIN t3)." };
 		TestUtils.testSentences(txts, pm, 1);
+
+	}
+
+	@Test
+	public void testNegatives() throws Exception {
+
+		String[] txts = { "This was previously working fine for months.", "previously referenced mod_cache.",
+				"I thought they were already removed, we have talked about this in the past (can't remember who it was), and I thought we agreed we would remove the API docs, or at least mark as deprecated, so it wouldn't cause confusion.",
+				"This was working before, I just noticed it happening today.",
+				"Before it opens, it starts the listening sockets and also starts rotatelogs itself." };
+		TestUtils.testSentences(txts, pm, 0);
 
 	}
 }
