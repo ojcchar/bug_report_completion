2de8fb31001bf0571372bec6cd9d80d287bc8b0d
Laura Moreno <lvmorenoc@gmail.com>
2016-08-22 23:15:27 -0600

    some fixes

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/AfterNegativePM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/AfterNegativePM.java
@@ -21,57 +21,66 @@ public class AfterNegativePM extends ObservedBehaviorPatternMatcher {
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 		List<Token> tokens = sentence.getTokens();
-		List<Integer> afters = findTermsInTokens(AFTER, tokens);
 
-		if (!afters.isEmpty()) {
-			List<Sentence> subSentences = findSubSentences(sentence, afters);
+		// split sentences based on "."
+		List<Sentence> superSentences = findSubSentences(sentence, findPeriod(tokens));
 
-			// if there is a sentence before the "after" term, skip it.
-			for (int i = afters.get(0) > 0 ? 1 : 0; i < subSentences.size(); i++) {
-				Sentence subSentece = subSentences.get(i);
+		for (Sentence superSentence : superSentences) {
 
-				List<Integer> punct = findPunctuation(subSentece.getTokens());
+			List<Integer> afters = findTermsInTokens(AFTER, superSentence.getTokens());
 
-				// if there is no punctuation, more than one verb in the sentence would indicate that right after the
-				// "after" there is a sentence and not a noun
-				if (punct.isEmpty()) {
-					List<Integer> verbs = findVerbs(subSentece.getTokens());
-					if (verbs.size() > 2) {
-						for (PatternMatcher pm : NEGATIVE_PMS) {
-							int match = pm.matchSentence(subSentences.get(i));
-							if (match == 1) {
-								return 1;
+			if (!afters.isEmpty()) {
+				// split sentences based on "after"
+				List<Sentence> subSentences = findSubSentences(superSentence, afters);
+
+				// if there is a sentence before the "after" term, skip it.
+				for (int i = afters.get(0) > 0 ? 1 : 0; i < subSentences.size(); i++) {
+					Sentence subSentece = subSentences.get(i);
+
+					List<Integer> punct = findPunctuation(subSentece.getTokens());
+
+					// if there is no punctuation, more than one verb in the sentence would indicate that right after
+					// the "after" there is a sentence and not a noun
+					if (punct.isEmpty()) {
+						List<Integer> verbs = findVerbs(subSentece.getTokens());
+						if (verbs.size() > 1) {
+							for (PatternMatcher pm : NEGATIVE_PMS) {
+								int match = pm.matchSentence(subSentences.get(i));
+								if (match == 1) {
+									return 1;
+								}
 							}
 						}
 					}
-				}
-				// if there is punctuation, we make sure that there is a sentence right after the "after", and a
-				// negative sentence after that. We discount phrases that would make a double negation such as the
-				// second part of "After tabbing or otherwise navigating through the autocomplete list for the location
-				// bar, the last entry selected appears in the location bar but is not loaded (this is correct)."
-				else {
-					List<Sentence> subSubSentences = findSubSentences(subSentece, punct);
-					if (!findVerbs(subSubSentences.get(0).getTokens()).isEmpty()) {
-						for (int j = 1; j < subSubSentences.size(); j++) {
-							boolean notDoubleNeg = true;
-							for (PatternMatcher pm : DOUBLE_NEG) {
-								int match = pm.matchSentence(subSubSentences.get(j));
-								if (match == 1) {
-									notDoubleNeg = false;
-								}
-							}
-							if (notDoubleNeg) {
-								for (PatternMatcher pm : NEGATIVE_PMS) {
+					// if there is punctuation, make sure that there is a sentence (not just nouns) right after the
+					// "after", and a negative sentence after that. We discount phrases that would make a double
+					// negation such as the second part of "After tabbing or otherwise navigating through the
+					// autocomplete list for the location bar, the last entry selected appears in the location bar but
+					// is not loaded (this is correct)."
+					else {
+						List<Sentence> subSubSentences = findSubSentences(subSentece, punct);
+						if (!findVerbs(subSubSentences.get(0).getTokens()).isEmpty()) {
+							for (int j = 1; j < subSubSentences.size(); j++) {
+								boolean notDoubleNeg = true;
+								for (PatternMatcher pm : DOUBLE_NEG) {
 									int match = pm.matchSentence(subSubSentences.get(j));
 									if (match == 1) {
-										return 1;
+										notDoubleNeg = false;
+									}
+								}
+								if (notDoubleNeg) {
+									for (PatternMatcher pm : NEGATIVE_PMS) {
+										int match = pm.matchSentence(subSubSentences.get(j));
+										if (match == 1) {
+											return 1;
+										}
 									}
 								}
 							}
 						}
 					}
-				}
 
+				}
 			}
 		}
 		return 0;
@@ -88,11 +97,27 @@ public class AfterNegativePM extends ObservedBehaviorPatternMatcher {
 		return symbols;
 	}
 
+	private List<Integer> findPeriod(List<Token> tokens) {
+		List<Integer> symbols = new ArrayList<>();
+		for (int i = 0; i < tokens.size(); i++) {
+			Token token = tokens.get(i);
+			if (token.getWord().equals(".")) {
+				symbols.add(i);
+			}
+		}
+		return symbols;
+	}
+
 	private List<Integer> findVerbs(List<Token> tokens) {
 		List<Integer> symbols = new ArrayList<>();
 		for (int i = 0; i < tokens.size(); i++) {
 			Token token = tokens.get(i);
 			if (token.getGeneralPos().equals("VB")) {
+				// ignore the to-infinitive form
+				if (i - 1 >= 0 && tokens.get(i - 1).getWord().equalsIgnoreCase("to")) {
+					continue;
+
+				}
 				symbols.add(i);
 			}
 		}
