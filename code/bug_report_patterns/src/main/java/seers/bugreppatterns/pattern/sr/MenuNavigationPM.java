6132c383282eaefc0a2bc5ac812780df4aed1ab5
Oscar Chaparro <ojcch1@gmail.com>
2016-08-23 20:15:51 -0500

    S_SR_MENU_NAV implemented/tested

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/MenuNavigationPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/MenuNavigationPM.java
@@ -1,46 +1,99 @@
 package seers.bugreppatterns.pattern.sr;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import seers.bugreppatterns.pattern.StepsToReproducePatternMatcher;
-import seers.textanalyzer.TextProcessor;
 import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
 
 public class MenuNavigationPM extends StepsToReproducePatternMatcher {
 
+	public final static String[] SEPARATORS = { "->", "-", "|", "--", ">" };
+
+	public final static String[] GUI_TERMS = { "window", "menu", "option", "preference", "tool", "setting", "checkbox",
+			"list", "panel", "table", "project", "edit", "download", "section", "page", "homepage", "screen" };
+
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-		// return 0;
-		String txt = TextProcessor.getStringFromLemmas(sentence);
-		txt = removeParentheses(txt);
-		boolean match = checkForSeparators(txt);
+		List<List<Token>> clauses = null;
 
-		return match ? 1 : 0;
-	}
-
-	private String removeParentheses(String txt) {
-		String txtBuf = txt;
-		for (int i = 0; i < TextProcessor.PARENTHESIS.length; i++) {
-			String p = TextProcessor.PARENTHESIS[i].toLowerCase();
-			txtBuf = txtBuf.replace(p, "");
-		}
-		return txtBuf;
-	}
-
-	public final static String[] SEPARATORS_REGEX = { " - > ", " - ", " \\| ", " -- " };
-
-	// TODO: we should check for separator manually, token by token, and avoid
-	// cases like "rpm -i -l"
-	private boolean checkForSeparators(String txt) {
-		for (int i = 0; i < SEPARATORS_REGEX.length; i++) {
-			String sep = SEPARATORS_REGEX[i];
-			String[] split = txt.split(sep);
-			if (split.length > 2 && !Arrays.stream(split).anyMatch(s -> s.trim().isEmpty())) {
-				return true;
+		// separate the sentence into clauses, by any of the defined separators
+		List<Token> tokens2 = sentence.getTokens();
+		for (int i = 0; i < SEPARATORS.length; i++) {
+			String separator = SEPARATORS[i];
+			clauses = getElementsBySeparator(tokens2, separator);
+			if (clauses.size() != 1) {
+				break;
 			}
 		}
 
-		return false;
+		// -----------------------
+
+		if (clauses.size() > 1) {
+			int numClausesWithGuiTerms = 0;
+
+			// check for gui terms
+			for (List<Token> elemTokens : clauses) {
+				if (Arrays.stream(GUI_TERMS)
+						.anyMatch(guiTerm -> elemTokens.stream().anyMatch(t -> t.getLemma().equalsIgnoreCase(guiTerm)
+								|| t.getLemma().toLowerCase().startsWith(guiTerm)))) {
+					numClausesWithGuiTerms++;
+				}
+			}
+
+			if (numClausesWithGuiTerms > 1) {
+				return 1;
+			}
+		}
+
+		return 0;
+
+	}
+
+	private List<List<Token>> getElementsBySeparator(List<Token> tokens, String separator) {
+
+		List<List<Token>> clauses = new ArrayList<>();
+		List<Token> clause = new ArrayList<>();
+		for (int i = 0; i < tokens.size();) {
+			Token token = tokens.get(i);
+
+			String lemma = getLemmas(tokens, i, separator.length());
+			if (lemma.equals(separator)) {
+				if (!clause.isEmpty()) {
+					clauses.add(clause);
+				}
+
+				clause = new ArrayList<>();
+				i += separator.length();
+			} else {
+				clause.add(token);
+				i++;
+			}
+		}
+		if (!clause.isEmpty()) {
+			clauses.add(clause);
+		}
+
+		return clauses;
+	}
+
+	private String getLemmas(List<Token> tokens, int i, int length) {
+
+		int toIndex = i + length;
+		List<Token> subList;
+		if (toIndex < tokens.size()) {
+			subList = tokens.subList(i, toIndex);
+		} else {
+			subList = tokens.subList(i, tokens.size());
+		}
+
+		StringBuffer buf = new StringBuffer();
+		for (Token token : subList) {
+			buf.append(token.getLemma());
+		}
+		return buf.toString();
 	}
 
 }
--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/MenuNavigationPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/MenuNavigationPMTest.java
@@ -1,14 +1,9 @@
 package seers.bugreppatterns.pattern.sr;
 
-import static org.junit.Assert.assertFalse;
-
-import java.util.List;
-
 import org.junit.Test;
 
 import seers.bugreppatterns.main.BaseTest;
-import seers.textanalyzer.TextProcessor;
-import seers.textanalyzer.entity.Sentence;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 public class MenuNavigationPMTest extends BaseTest {
 
@@ -17,7 +12,17 @@ public class MenuNavigationPMTest extends BaseTest {
 	}
 
 	@Test
+	public void testPositives() throws Exception {
+
+		String[] txts = {
+				"Adding a batch file (.bat) as a helper application seems to break the Download Actions component (in Tools | Options | Downloads | View & Edit Actions)." };
+		TestUtils.testSentences(txts, pm, 1);
+
+	}
+
+	@Test
 	public void testNegative() throws Exception {
+
 		String[] txts = {
 				"In the CVS Repository Exploring perspective, if you select \"Compare With\" on a\n	branch or version, it eventually opens a \"Structured Compare\" window showing all\n	the changes.",
 				"This means you cannot benefit from the Synchronize view's changeset\nsupport -- you can access diffs, but no commit log information.",
@@ -25,20 +30,11 @@ public class MenuNavigationPMTest extends BaseTest {
 				"The commit failed because the command claimed that several files were not up-to-date.",
 				"However, it would be great if upon a double click there, the Project\nproperties-Java Build Path-Libraries would open the concerned library where I\ncould attach the source",
 				"However_ the  New title set within....  is the title on the page as the fbml parser is ignoring the fb:js-string tag and using the title encapsulated within\n    &lt;fb:title&gt;Initial Title&lt;/fb:title&gt;\n    &lt;fb:js-string var= newtitle &gt;\n        &lt;fb:title&gt;New title set within fb js-string variable&lt;/fb:title&gt;\n    &lt;/fb:js-string&gt;",
-				"When I ran htpasswd a Segment Fault occurr." };
+				"When I ran htpasswd a Segment Fault occurr.", "| SSD | btrfs | -s btrfs | 8s |",
+				"echo \"echo hi\" | docker run -i ubuntu | print" };
 
-		boolean fail = false;
-		for (int i = 0; i < txts.length; i++) {
-			String txt = txts[i];
-			List<Sentence> sentences = TextProcessor.processText(txt);
-			int m = pm.matchSentence(new Sentence("0", TextProcessor.getAllTokens(sentences)));
-			if (m != 0) {
-				System.out.println("\n Fail for (" + i + "): \"" + txt + "\"");
-				fail = true;
-			}
-		}
+		TestUtils.testSentences(txts, pm, 0);
 
-		assertFalse(fail);
 	}
 
 }
