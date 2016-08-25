1a65b5ca5d2b779c941c8d5fbd062710e1df6deb
Laura Moreno <lvmorenoc@gmail.com>
2016-08-25 11:41:30 -0600

    S_OB_AS_IF implemented and tested

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/AsIfPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class AsIfPMTest extends BaseTest {
+	public AsIfPMTest() {
+		pm = new AsIfPM();
+	}
+
+}
