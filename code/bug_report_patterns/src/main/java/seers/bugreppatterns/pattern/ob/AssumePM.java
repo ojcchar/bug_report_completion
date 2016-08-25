945c65a69d077a81852778fdda176ecbaccd4cd7
Laura Moreno <lvmorenoc@gmail.com>
2016-08-25 15:40:28 -0600

    S_OB_ASSUME implemented and tested

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/AssumePM.java
@@ -0,0 +1,64 @@
+package seers.bugreppatterns.pattern.ob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class AssumePM extends ObservedBehaviorPatternMatcher {
+
+	private static String ASSUME = "assume";
+
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		List<Integer> assumes = findAssume(tokens);
+
+		if (!assumes.isEmpty()) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private List<Integer> findAssume(List<Token> tokens) {
+		List<Integer> assumes = new ArrayList<Integer>();
+
+		for (int i = 1; i < tokens.size(); i++) {
+			Token current = tokens.get(i);
+
+			// Look for every "assume"
+			if (current.getGeneralPos().equals("VB") && current.getLemma().equals(ASSUME)) {
+				Token previous = tokens.get(i - 1);
+
+				// The right "assume": the one that comes after a noun
+				if (previous.getGeneralPos().equals("NN")) {
+					assumes.add(i);
+				}
+				// Sometimes there are parenthesis in between. We skip their content and look for a noun before the
+				// parenthesis
+				else if (previous.getGeneralPos().equals("-RRB-")) {
+					int j = i - 2;
+					while (j >= 0) {
+						if (tokens.get(j).getGeneralPos().equals("-LRB-")) {
+							break;
+						}
+						j--;
+					}
+
+					if (j > 0) {
+						if (tokens.get(j - 1).getGeneralPos().equals("NN")) {
+							assumes.add(i);
+						}
+					}
+				}
+
+			}
+
+		}
+		return assumes;
+	}
+
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/AssumePMTest.java
@@ -0,0 +1,23 @@
+package seers.bugreppatterns.pattern.ob;
+
+import org.junit.Test;
+
+import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
+
+public class AssumePMTest extends BaseTest {
+	public AssumePMTest() {
+		pm = new AssumePM();
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+		String[] negs = {
+				"Now if you restart the Docker daemon, Docker thinks the container is gone (I assume because the PID is gone) and then it will start a new one but the old container cgroup still exists.",
+				"I assume this wiki page just isn't maintained any more so maybe you want to link to a more recent plan?",
+				"I am assuming this wiki page just isn't maintained any more so maybe you want to link to a more recent plan?"};
+
+		TestUtils.testSentences(negs, pm, 0);
+	}
+
+}
