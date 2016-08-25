7be24b22b6a9969cad07b19bb89b52d67554ccdc
Laura Moreno <lvmorenoc@gmail.com>
2016-08-24 23:17:58 -0600

    S_OB_WITHOUT implemented and tested. We reached 100 pattern implementations!!!!

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/WithoutPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class WithoutPMTest extends BaseTest {
+	public WithoutPMTest() {
+		pm = new WithoutPM();
+	}
+
+}
