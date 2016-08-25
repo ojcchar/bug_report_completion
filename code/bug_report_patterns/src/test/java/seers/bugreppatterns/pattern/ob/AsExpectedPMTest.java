a3cec1a63a5fabef1b5b05726d6a512887776d65
Laura Moreno <lvmorenoc@gmail.com>
2016-08-25 11:32:41 -0600

    S_OB_AS_EXPECTED implemented and tested

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/AsExpectedPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class AsExpectedPMTest extends BaseTest {
+	public AsExpectedPMTest() {
+		pm = new AsExpectedPM();
+	}
+
+}
