bda32d11430aba3fbca3d5b90f4fe33adb9c6665
Laura Moreno <lvmorenoc@gmail.com>
2016-08-25 11:59:46 -0600

    S_OB_AS_RESULT implemented and tested

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/AsResultPMTest.java
@@ -0,0 +1,10 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+public class AsResultPMTest extends BaseTest {
+	public AsResultPMTest() {
+		pm = new AsResultPM();
+	}
+
+}
