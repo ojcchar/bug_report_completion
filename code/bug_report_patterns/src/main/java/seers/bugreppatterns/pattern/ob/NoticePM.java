ad332fdba66a7b20e28334608a40411ba399637f
Laura Moreno <lvmorenoc@gmail.com>
2016-08-26 15:47:16 -0600

    S_OB_NOTICE implemented and tested

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NoticePM.java
@@ -0,0 +1,81 @@
+package seers.bugreppatterns.pattern.ob;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class NoticePM extends ObservedBehaviorPatternMatcher {
+
+	private final static String[] NOTICE_TERMS = { "notice", "see" };
+
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		List<Integer> finds = findFinds(tokens);
+
+		if (!finds.isEmpty()) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private List<Integer> findFinds(List<Token> tokens) {
+		List<Integer> finds = new ArrayList<Integer>();
+
+		for (int i = 0; i < tokens.size(); i++) {
+			Token current = tokens.get(i);
+
+			// Look for every "notice"
+			if (Arrays.stream(NOTICE_TERMS).anyMatch(t -> current.getLemma().equals(t))) {
+
+				// The right "notice"
+				if (i - 1 >= 0 && i < tokens.size() - 1) {
+					Token previous = tokens.get(i - 1);
+
+					// the one preceded by a pronoun
+					if ((current.getPos().equals("VBD") || current.getPos().equals("VBP")
+							|| current.getPos().equals("VBN"))
+							&& (previous.getGeneralPos().equals("PRP")
+									|| previous.getWord().toLowerCase().equals("i"))) {
+						finds.add(i);
+					}
+					// the one preceded by another verb/modal that is preceded by a pronoun
+					else if ((previous.getGeneralPos().equals("VB") || (previous.getGeneralPos().equals("MD") && !isEBModal(tokens.subList(i-1, i))))
+							&& i - 2 >= 0) {
+						if (tokens.get(i - 2).getGeneralPos().equals("PRP")) {
+							finds.add(i);
+						}
+					}
+					// the one preceded by a conjunction, which is preceded at some point by a pronoun
+					else if (previous.getGeneralPos().equals("CC")) {
+						for (int j = i - 2; j >= 0; j--) {
+							if (tokens.get(j).getGeneralPos().equals("PRP")) {
+								finds.add(i);
+							}
+						}
+					}
+					// the one starting a sentence
+					else if (previous.getLemma().equals(",") || previous.getLemma().equals("-")) {
+						finds.add(i);
+					}
+					// the one preceded by after
+					else if (previous.getLemma().equals("after")) {
+						finds.add(i);
+					}
+				}
+				// the one starting a sentence
+				else if (i == 0  && i < tokens.size() - 1) {
+					finds.add(i);
+				}
+
+			}
+
+		}
+		return finds;
+	}
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/NoticePMTest.java
@@ -0,0 +1,20 @@
+package seers.bugreppatterns.pattern.ob;
+
+import org.junit.Test;
+
+import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
+
+public class NoticePMTest extends BaseTest {
+	public NoticePMTest() {
+		pm = new NoticePM();
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+		String[] negs = {
+				"I should notice that", "I noticed" };
+
+		TestUtils.testSentences(negs, pm, 0);
+	}
+}
