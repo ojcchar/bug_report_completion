ea717be6c3e2e98f34b636c43193bf6208a85136
Laura Moreno <lvmorenoc@gmail.com>
2016-08-26 13:09:20 -0600

    S_OB_OBSERVED implemented and tested

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ObservePM.java
@@ -0,0 +1,61 @@
+package seers.bugreppatterns.pattern.ob;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+public class ObservePM extends ObservedBehaviorPatternMatcher {
+
+	private static String OBSERVE = "observe";
+
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
+		List<Integer> observes = findObserve(tokens);
+
+		if (!observes.isEmpty()) {
+			return 1;
+		}
+
+		return 0;
+	}
+
+	private List<Integer> findObserve(List<Token> tokens) {
+		List<Integer> observe = new ArrayList<Integer>();
+
+		for (int i = 0; i < tokens.size(); i++) {
+			Token current = tokens.get(i);
+
+			// Look for every "observe"
+			if (current.getGeneralPos().equals("VB") && current.getLemma().equals(OBSERVE)) {
+
+				// The right "observe"
+				if (current.getPos().equals("VBP") || current.getPos().equals("VB") || current.getPos().equals("VBD")
+						|| current.getPos().equals("VBN")) {
+
+					// the one in the first position
+					if (i == 0) {
+						observe.add(i);
+					} 
+					// the one followed by ":" or "that"
+					else if (i + 1 < tokens.size() && (tokens.get(i + 1).getLemma().equals(":")
+							|| tokens.get(i + 1).getLemma().equals("that"))) {
+						observe.add(i);
+					} 
+					// the one preceded by an item marker, cardinal number, or pronoun
+					else if (i - 1 >= 0 && (tokens.get(i - 1).getGeneralPos().equals("LS")
+							|| tokens.get(i - 1).getGeneralPos().equals("CD")
+							|| tokens.get(i - 1).getGeneralPos().equals("PRP"))) {
+						observe.add(i);
+					}
+				}
+			}
+
+		}
+		return observe;
+	}
+
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ObservePMTest.java
@@ -0,0 +1,34 @@
+package seers.bugreppatterns.pattern.ob;
+
+import org.junit.Test;
+
+import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
+
+public class ObservePMTest extends BaseTest {
+	public ObservePMTest() {
+		pm = new ObservePM();
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+		String[] negs = {
+				"When the performance hit is observed we notice that there are repeated continuous requests to our cross-domain receiver file (as passed into FB.init()).",
+				"The Data Entry Statistics module (version 1.3) includes voided encounters and observations in the table of data entry statistics."};
+
+		TestUtils.testSentences(negs, pm, 0);
+	}
+
+	@Test
+	public void testPositive() throws Exception {
+		String[] negs = {
+				"We observed that in the free membership registration page all mandatory fields are not marked with *.",
+				"However, I observe the same error with pure IPv4.",
+				"Observed: Error response from daemon: Cannot start container ab59989666093c50b0f0b137096ff562c2cdd94f162b351b57a5fcd0217c7682: [8] System error: Invalid unit type.",
+				"If you observe the output of execution of the TestCX class, you see an error to second user (Clayton) in return of method getClass().",
+				"Observed: The refresh fails to grab the data and the stats page reflects a bunch of nonsense (/0 error)" };
+
+		TestUtils.testSentences(negs, pm, 1);
+	}
+
+}
