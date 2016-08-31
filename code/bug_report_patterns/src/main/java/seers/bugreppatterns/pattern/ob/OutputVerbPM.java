22cad4802ca907052ade37c0a4dcaa1f4794446c
Oscar Chaparro <ojcch1@gmail.com>
2016-08-30 20:50:12 -0500

    S_OB_OUTPUT_VERB implemented/tested

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/OutputVerbPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/OutputVerbPM.java
@@ -1,28 +1,69 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
 import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
 
 /**
  * Matcher for S_OB_OUTPUT_VERB
  */
 public class OutputVerbPM extends ObservedBehaviorPatternMatcher {
-    public static final Set<String> OUTPUT_VERBS = Arrays.asList("output", "display", "show",
-            "return", "report")
-            .stream()
-            .collect(Collectors.toSet());
+	public static final Set<String> OUTPUT_VERBS = Arrays.asList("output", "display", "show", "return", "report")
+			.stream().collect(Collectors.toSet());
 
-    @Override
-    public int matchSentence(Sentence sentence) throws Exception {
-        // TODO: uncomment line to include only verbs. Commented because some are incorrectly tagged
-        boolean match = sentence.getTokens().stream().anyMatch(
-                token -> // token.getGeneralPos().equals("VB") &&
-                        OUTPUT_VERBS.contains(token.getLemma()));
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		List<Token> tokens = sentence.getTokens();
 
-        return match ? 1 : 0;
-    }
+		List<Integer> verbs = getVerbs(tokens);
+		for (Integer verbIdx : verbs) {
+			Token verbToken = tokens.get(verbIdx);
+			if (verbToken.getGeneralPos().equals("VB")) {
+				return 1;
+			} else {
+				if (verbIdx - 1 >= 0 && verbIdx + 1 < tokens.size()) {
+					Token prevToken = tokens.get(verbIdx - 1);
+					Token nextToken = tokens.get(verbIdx + 1);
+
+					// no surrounding verbs
+					// avoid cases like "the output"
+					// avoid cases like "output display goes "
+					if (!prevToken.getGeneralPos().equals("VB") && !nextToken.getGeneralPos().equals("VB")
+							&& !prevToken.getGeneralPos().equals("DT") && !OUTPUT_VERBS.contains(prevToken.getLemma())
+							&& !OUTPUT_VERBS.contains(nextToken.getLemma())) {
+
+						// avoid "is not output"
+						if (verbIdx - 2 >= 0) {
+							Token prevToken2 = tokens.get(verbIdx - 2);
+							if (!prevToken2.getLemma().equals("be") && !prevToken.getLemma().equals("not")) {
+								return 1;
+							}
+						} else {
+							return 1;
+						}
+					}
+				}
+			}
+		}
+
+		return 0;
+	}
+
+	private List<Integer> getVerbs(List<Token> tokens) {
+
+		List<Integer> mainToks = new ArrayList<>();
+		for (int i = 0; i < tokens.size(); i++) {
+			Token token = tokens.get(i);
+			if (OUTPUT_VERBS.contains(token.getLemma())) {
+				mainToks.add(i);
+			}
+		}
+		return mainToks;
+	}
 }
--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/OutputVerbPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/OutputVerbPMTest.java
@@ -1,14 +1,36 @@
 package seers.bugreppatterns.pattern.ob;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 /**
  * Created by juan on 8/11/16.
  */
 public class OutputVerbPMTest extends BaseTest {
-    {
-        pm = new OutputVerbPM();
-    }
-    
-    
+	{
+		pm = new OutputVerbPM();
+	}
+
+	@Test
+
+	public void testPositives() throws Exception {
+		String[] txts = {
+				"In this example the `strace` output shows that Entrypoint: [/bin/bash,-l]` was sent to the server, but inspecting the resulting image shows `Entrypoint: null`.", };
+		TestUtils.testSentences(txts, pm, 1);
+	}
+
+	@Test
+	public void testNegatives() throws Exception {
+		String[] txts = { "The output is\noriginal: http://host/my%20path/\nparsed:   http://host/my%2520path/",
+				"Persistence.generateSchema - the output file contains duplicated DDL statements",
+				"sometimes favicons in url results are blank, need to use the favicon service",
+				"Using LibreOffice Draw 4.1.0.4 in Fedora 19 x86 64bit or Widnows 7 64bit, EPS clipart is not output in printing or PDF export.",
+				"Sample code to attach to a canvas page input field is below_ output display goes to elements with IDs keydown_ keyup_ and keypress.",
+				"the correct output is: class com.ws.model.entities.Mensagem",
+				"the result must be retrieved in 2 dimensional array of Object(Object[][])" };
+		TestUtils.testSentences(txts, pm, 0);
+	}
+
 }
