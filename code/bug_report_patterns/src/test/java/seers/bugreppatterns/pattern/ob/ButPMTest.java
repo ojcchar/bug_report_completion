e5d6b26f121ca5304519d1ffb50936377ef6dfb9
Laura Moreno <lvmorenoc@gmail.com>
2016-09-08 14:08:23 -0600

    some tests after coding review

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ButPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ButPMTest.java
@@ -1,11 +1,21 @@
 package seers.bugreppatterns.pattern.ob;
 
-import seers.bugreppatterns.main.BaseTest;
+import org.junit.Test;
 
-public class ButPMTest extends BaseTest{
+import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
+
+public class ButPMTest extends BaseTest {
 
 	public ButPMTest() {
 		pm = new ButPM();
 	}
 
+	@Test
+	public void testPositive() throws Exception {
+		String[] sentences = {
+
+				"I can, however, remove the mistake identifier via the long form by voiding it.", };
+		TestUtils.testSentences(sentences, pm, 1);
+	}
 }
