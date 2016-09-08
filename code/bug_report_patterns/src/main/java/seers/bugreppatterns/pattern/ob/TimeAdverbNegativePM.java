70e7f8efa5e1e85d445a2aa15ddf7318e72eb7fa
Laura Moreno <lvmorenoc@gmail.com>
2016-09-08 13:46:17 -0600

    revised S_OB_ADV_TIME_* patterns

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/TimeAdverbNegativePM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/TimeAdverbNegativePM.java
@@ -15,42 +15,39 @@ import seers.textanalyzer.entity.Token;
  * Matcher for S_OB_ADV_TIME_NEG.
  */
 public class TimeAdverbNegativePM extends ObservedBehaviorPatternMatcher {
-    public static final Set<String> TIME_ADVERBS = Arrays.asList("currently", "usually",
-            "frequently", "generally", "normally", "occasionally", "often", "regularly",
-            "sometimes", "again", "now")
-            .stream()
-            .collect(Collectors.toSet());
+	public static final Set<String> TIME_ADVERBS = Arrays.asList("currently", "again", "now").stream()
+			.collect(Collectors.toSet());
 
-    /**
-     * Adverbs that only count as a match if they are in the first position.
-     */
-    public static final Set<String> FIRST_POS_TIME_ADVERBS = Arrays.asList("then", "now", "again")
-            .stream()
-            .collect(Collectors.toSet());
+	/**
+	 * Adverbs that only count as a match if they are in the first position.
+	 */
+	public static final Set<String> FIRST_POS_TIME_ADVERBS = Arrays.asList("then", "now", "again").stream()
+			.collect(Collectors.toSet());
 
-    public static final String[] ADVERBIAL_TIME_CLAUSES = {"as soon as", "right now", "once again",
-            "for the moment", "up to now", "as it stand", "for a brief moment", "for a moment"};
+	public static final String[] ADVERBIAL_TIME_CLAUSES = { "as soon as", "right now", "once again", "for the moment",
+			"up to now", "as it stand", "for a brief moment", "for a moment", "in this moment" };
 
-    @Override
-    public int matchSentence(Sentence sentence) throws Exception {
-        List<Token> tokens = sentence.getTokens();
+	@Override
+	public int matchSentence(Sentence sentence) throws Exception {
+		
+		List<Token> tokens = sentence.getTokens();
 
-        if (!FIRST_POS_TIME_ADVERBS.contains(tokens.get(0).getLemma()) &&
-                !tokens.stream().anyMatch(token -> TIME_ADVERBS.contains(token.getLemma()))) {
+		if (!FIRST_POS_TIME_ADVERBS.contains(tokens.get(0).getLemma())
+				&& !tokens.stream().anyMatch(token -> TIME_ADVERBS.contains(token.getLemma()))) {
 
-            String text = TextProcessor.getStringFromLemmas(sentence);
+			String text = TextProcessor.getStringFromLemmas(sentence);
 
-            if (!Arrays.stream(ADVERBIAL_TIME_CLAUSES).anyMatch(text::contains)) {
-                return 0;
-            }
-        }
+			if (!Arrays.stream(ADVERBIAL_TIME_CLAUSES).anyMatch(text::contains)) {
+				return 0;
+			}
+		}
 
-        for (PatternMatcher pm : ButNegativePM.NEGATIVE_PMS) {
-            if (pm.matchSentence(sentence) == 1) {
-                return 1;
-            }
-        }
+		for (PatternMatcher pm : ButNegativePM.NEGATIVE_PMS) {
+			if (pm.matchSentence(sentence) == 1) {
+				return 1;
+			}
+		}
 
-        return 0;
-    }
+		return 0;
+	}
 }
--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/TimeAdverbNegativePMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/TimeAdverbNegativePMTest.java
@@ -1,12 +1,23 @@
 package seers.bugreppatterns.pattern.ob;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 /**
  * Created by juan on 8/11/16.
  */
 public class TimeAdverbNegativePMTest extends BaseTest {
-    {
-        pm = new TimeAdverbNegativePM();
-    }
+	{
+		pm = new TimeAdverbNegativePM();
+	}
+
+	@Test
+	public void testPositive() throws Exception {
+		String[] sentences = {
+					"### Actual behavior Right now `timestamp`, being used as a sorting criteria, even when sometimes a value other than an actual timestamp is assigned to such `timestamp` attribute, which is misleading." };
+
+		TestUtils.testSentences(sentences, pm, 1);
+	}
 }
