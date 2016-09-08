4ff83d6be82e8fce0c1450c1b1b68c3632120ce5
Laura Moreno <lvmorenoc@gmail.com>
2016-09-08 14:13:04 -0600

    some tests after coding review

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/TimeAdverbPositivePMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/TimeAdverbPositivePMTest.java
@@ -1,12 +1,23 @@
 package seers.bugreppatterns.pattern.ob;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 /**
  * Created by juan on 8/15/16.
  */
 public class TimeAdverbPositivePMTest extends BaseTest {
-    {
-        pm = new TimeAdverbPositivePM();
-    }
+	{
+		pm = new TimeAdverbPositivePM();
+	}
+
+	@Test
+	public void testPositive() throws Exception {
+		String[] sentences = {
+
+				"With the introduction of the function: ap_register_rewrite_mapfunc every Apache Module Writer can now create its own internal RewriteMap.", };
+		TestUtils.testSentences(sentences, pm, 1);
+	}
 }
