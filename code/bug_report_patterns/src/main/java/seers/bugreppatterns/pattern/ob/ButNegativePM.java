b72dd92df742f72a86b315f18cee3edbc8f7bc33
Laura Moreno <lvmorenoc@gmail.com>
2016-09-08 21:03:31 -0600

    S_OB_BUT_* patterns updated.

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ButNegativePM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ButNegativePM.java
@@ -1,5 +1,6 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,31 +13,35 @@ public class ButNegativePM extends ObservedBehaviorPatternMatcher {
 
 	public final static PatternMatcher[] NEGATIVE_PMS = { new NegativeAuxVerbPM(), new NegativeVerbPM(),
 			new NoLongerPM(), new VerbErrorPM(), new ThereIsNoPM(), new NegativeAdjOrAdvPM(), new UnableToPM(),
-			new VerbNoPM(), new ProblemInPM(), new NoNounPM(), new ErrorTermSubjectPM(),
-			new ErrorNounPhrasePM() };
+			new VerbNoPM(), new ProblemInPM(), new NoNounPM(), new ErrorTermSubjectPM(), new ErrorNounPhrasePM() };
 
 	final static String[] NEGATIVE_TERMS = { "no", "nothing", "not", "never" };
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 
-		List<Token> tokens2 = sentence.getTokens();
-		List<Integer> buts = findTermsInTokens(CONTRAST_TERMS, tokens2);
-		for (Integer but : buts) {
+		List<Sentence> subSentences = findSubSentences(sentence, findPunctuation(sentence.getTokens()));
 
-			if (but + 1 < tokens2.size()) {
-				Token nextToken = tokens2.get(but + 1);
-				if (Arrays.stream(NEGATIVE_TERMS).anyMatch(t -> nextToken.getLemma().equals(t))) {
-					return 1;
+		for (Sentence subSentence : subSentences) {
+			List<Token> subTokens = subSentence.getTokens();
+			List<Integer> buts = findTermsInTokens(CONTRAST_TERMS, subTokens);
+
+			for (Integer but : buts) {
+
+				if (but + 1 < subTokens.size()) {
+					Token nextToken = subTokens.get(but + 1);
+					if (Arrays.stream(NEGATIVE_TERMS).anyMatch(t -> nextToken.getLemma().equals(t))) {
+						return 1;
+					}
+
+					Sentence sentence2 = new Sentence(sentence.getId(), subTokens.subList(but + 1, subTokens.size()));
+					if (isNegative(sentence2)) {
+						return 1;
+					}
+
 				}
 
-				Sentence sentence2 = new Sentence(sentence.getId(), tokens2.subList(but + 1, tokens2.size()));
-				if(isNegative(sentence2)) {
-					return 1;
-				}
-				
 			}
-
 		}
 
 		return 0;
@@ -44,5 +49,16 @@ public class ButNegativePM extends ObservedBehaviorPatternMatcher {
 
 	private boolean isNegative(Sentence sentence) throws Exception {
 		return sentenceMatchesAnyPatternIn(sentence, NEGATIVE_PMS);
+	}
+
+	private List<Integer> findPunctuation(List<Token> tokens) {
+		List<Integer> symbols = new ArrayList<>();
+		for (int i = 0; i < tokens.size() - 1; i++) {
+			Token token = tokens.get(i);
+			if (token.getWord().equals(".") || token.getWord().equals(";") || token.getWord().equals("--")) {
+				symbols.add(i);
+			}
+		}
+		return symbols;
 	}
 }
--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ButPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ButPM.java
@@ -1,5 +1,6 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
@@ -7,30 +8,46 @@ import seers.bugreppatterns.pattern.PatternMatcher;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
-public class ButPM extends ObservedBehaviorPatternMatcher{
+public class ButPM extends ObservedBehaviorPatternMatcher {
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-		List<Token> tokens2 = sentence.getTokens();
-		List<Integer> buts = findTermsInTokens(CONTRAST_TERMS, tokens2);
 
-		PatternMatcher pmw = new WorksButPM();
-		int match1 = pmw.matchSentence(sentence);
-		if (match1 == 0) {
-			for (Integer but : buts) {
-				Sentence sentence2 = new Sentence(sentence.getId(),
-						tokens2.subList(but, tokens2.size()));
-				if(!isNegative(sentence2)) {
-					return 1;
+		List<Sentence> subSentences = findSubSentences(sentence, findPunctuation(sentence.getTokens()));
+
+		for (Sentence subSentence : subSentences) {
+			List<Token> subTokens = subSentence.getTokens();
+			List<Integer> buts = findTermsInTokens(CONTRAST_TERMS, subTokens);
+
+			PatternMatcher pmw = new WorksButPM();
+			int match1 = pmw.matchSentence(subSentence);
+			if (match1 == 0) {
+				for (Integer but : buts) {
+					Sentence sentence2 = new Sentence(subSentence.getId(),
+							subTokens.subList(but + 1, subTokens.size()));
+					if (!isNegative(sentence2)) {
+						return 1;
+					}
 				}
 			}
 		}
 
 		return 0;
 	}
-	
+
 	private boolean isNegative(Sentence sentence) throws Exception {
 		return sentenceMatchesAnyPatternIn(sentence, ButNegativePM.NEGATIVE_PMS);
 	}
-	
+
+	private List<Integer> findPunctuation(List<Token> tokens) {
+		List<Integer> symbols = new ArrayList<>();
+		for (int i = 0; i < tokens.size() - 1; i++) {
+			Token token = tokens.get(i);
+			if (token.getWord().equals(".") || token.getWord().equals(";") || token.getWord().equals("--")) {
+				symbols.add(i);
+			}
+		}
+		return symbols;
+	}
+
 }
