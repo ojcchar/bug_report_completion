e5055ee15ea684cbfe7af04746177404330152d0
Laura Moreno <lvmorenoc@gmail.com>
2016-09-10 18:28:03 -0600

    Revised VerbNoPM().

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/VerbNoPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/VerbNoPMTest.java
@@ -1,11 +1,28 @@
 package seers.bugreppatterns.pattern.ob;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 public class VerbNoPMTest extends BaseTest {
 
 	public VerbNoPMTest() {
 		pm = new VerbNoPM();
+	}
+
+	@Test
+	public void testNegatives() throws Exception {
+		String[] n = {};
+
+		TestUtils.testSentences(n, pm, 0);
+	}
+
+	@Test
+	public void testPositives() throws Exception {
+		String[] n = { "it returns with no error", "it returns with no name", "When we runs the script from an existing dos window the command prompt returns with no output." };
+
+		TestUtils.testSentences(n, pm, 1);
 	}
 
 }
