870cb16144903bc883b4ef5ce2c5369649859298
Laura Moreno <lvmorenoc@gmail.com>
2016-09-10 18:30:25 -0600

    list of negative terms updated.

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeTerms.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/NegativeTerms.java
@@ -2,10 +2,10 @@ package seers.bugreppatterns.pattern.ob;
 
 public class NegativeTerms {
 	final public static String[] ADJECTIVES = { "abnormal", "agressive", "ambiguous", "annoying", "bad", "bizarre",
-			"blank", "blocked", "bloken", "broken", "challenging", "confusing", "counter-intuitive", "corrupt", "corrupted",
-			"cpu-bound", "dead", "different", "dirty", "down", "duplicated", "empty", "enormous", "erroneous",
-			"excessive", "expensive", "extra", "faulty", "frustrating", "funny-looking", "garbled", "gone", "hard",
-			"ignored", "improper", "inaccessible", "inappropriate", "incorrect", "inconsitant", "inconsistent",
+			"blank", "blocked", "bloken", "broken", "challenging", "confusing", "counter-intuitive", "corrupt",
+			"corrupted", "cpu-bound", "dead", "different", "dirty", "down", "duplicated", "empty", "enormous",
+			"erroneous", "excessive", "expensive", "extra", "faulty", "frustrating", "funny-looking", "garbled", "gone",
+			"hard", "ignored", "improper", "inaccessible", "inappropriate", "incorrect", "inconsitant", "inconsistent",
 			"incredible", "indefinite", "inefficient", "infinite", "insecure", "invalid", "lost", "meaningless",
 			"messed", "mis-parsed", "misleading", "misplaced", "missing", "mistaken", "null", "odd", "off",
 			"off-center", "old", "orphaned", "out-of", "out-of-date", "outdated", "out-dated", "painful", "poor",
@@ -25,18 +25,18 @@ public class NegativeTerms {
 			"unreadably", "unresponsively", "unsecurely", "unstably", "weirdly", "wrongly" };
 
 	final public static String[] VERBS = { "affect", "blink", "block", "break", "bypass", "clear", "clash", "clobber",
-			"complain", "crash", "cut", "deadlock", "delay", "delete", "deny", "destroy", "die", "disappear", "duplicate",
-			"enforce", "erase", "fail", "flicker", "force", "forget", "freeze", "glitch", "grow", "hang", "ignore",
-			"increase", "interfere", "interrupt", "jerk", "jitter", "kill", "lack", "lose", "loose", "mess",
+			"complain", "crash", "cut", "deadlock", "delay", "delete", "deny", "destroy", "die", "disappear", "dismiss",
+			"duplicate", "enforce", "erase", "fail", "flicker", "force", "forget", "freeze", "glitch", "grow", "hang",
+			"ignore", "increase", "interfere", "interrupt", "jerk", "jitter", "kill", "lack", "lose", "loose", "mess",
 			"mishandle", "miss", "mute", "outdate", "overlap", "pause", "prohibit", "reduce", "refuse", "reject",
 			"reset", "rest", "restart", "revert", "reverse", "skip", "stick", "stop", "strip", "stuck up", "suffer",
 			"terminate", "throw", "time out", "trim", "truncate", "vanish", "terminate", "trim", "waste", "wipe" };
 
 	final public static String[] NOUNS = { "401", "404", "ambiguity", "block", "breakage", "bug", "chaos", "clash",
-			"collision", "conflict", "confusion", "corruption", "crash", "cut", "deadlock", "delay", "disaster", "duplicate",
-			"error", "errore", "exception", "failure", "fault", "frustration", "glitch", "grief", "inability", "ise",
-			"issue", "leak", "leakage", "lock", "loss", "malware", "mismatch", "misplace", "mistake", "nil", "nonsense",
-			"npe", "npes", "null", "omission", "overlap", "pain", "peek", "poor", "problem", "race", "rarity",
-			"runaway", "segfault", "segmentation", "spam", "stacktrace", "symptom", "trouble", "truncation", "typo",
-			"unavailability", "violation", "wait", "warn", "warning", "zombie" };
+			"collision", "conflict", "confusion", "corruption", "crash", "cut", "deadlock", "delay", "disaster",
+			"duplicate", "error", "errore", "exception", "failure", "fault", "frustration", "glitch", "grief",
+			"inability", "ise", "issue", "leak", "leakage", "lock", "loss", "malware", "mismatch", "misplace",
+			"mistake", "nil", "nonsense", "npe", "npes", "null", "omission", "overlap", "pain", "peek", "poor",
+			"problem", "race", "rarity", "runaway", "segfault", "segmentation", "spam", "stacktrace", "symptom",
+			"trouble", "truncation", "typo", "unavailability", "violation", "wait", "warn", "warning", "zombie" };
 }
