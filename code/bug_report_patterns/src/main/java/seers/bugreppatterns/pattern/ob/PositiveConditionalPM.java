4553463151db6c9bbb317e30d7a1fa17fb618dc0
Laura Moreno <lvmorenoc@gmail.com>
2016-09-12 15:18:08 -0600

    Clones removed.

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/PositiveConditionalPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/PositiveConditionalPM.java
@@ -3,7 +3,6 @@ package seers.bugreppatterns.pattern.ob;
 import java.util.List;
 
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
-import seers.bugreppatterns.pattern.PatternMatcher;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
@@ -21,7 +20,7 @@ public class PositiveConditionalPM extends ObservedBehaviorPatternMatcher {
 				if (ctp > 0) {
 					Sentence sentence2 = new Sentence(sentence.getId(), tokens.subList(0, ctp));
 					List<Token> tok2 = sentence2.getTokens();
-					if (!checkEBModal(tok2) && !checkNegative(sentence2)) {
+					if (!isEBModal(tok2) && !isNegative(sentence2)) {
 						return 1;
 					}
 				}
@@ -30,26 +29,8 @@ public class PositiveConditionalPM extends ObservedBehaviorPatternMatcher {
 		return 0;
 	}
 
-	private boolean checkNegative(Sentence sentence) throws Exception {
-
-		for (PatternMatcher pm : NegativeAfterPM.NEGATIVE_PMS) {
-			int match = pm.matchSentence(sentence);
-			if (match == 1) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private boolean checkEBModal(List<Token> tokens) {
-		for (int i = 0; i < tokens.size(); i++) {
-			if ((tokens.get(i).getLemma().equals("must") || tokens.get(i).getLemma().equals("need")
-					|| tokens.get(i).getLemma().equals("should"))) {
-				// check that it is not NEGATIVE
-				return true;
-			}
-		}
-		return false;
+	private boolean isNegative(Sentence sentence) throws Exception {
+		return sentenceMatchesAnyPatternIn(sentence, NegativeAfterPM.NEGATIVE_PMS);
 	}
 
 }
