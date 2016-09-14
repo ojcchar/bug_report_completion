4f52eff327374dc7ed585c71546bbf36f0b91a89
Laura Moreno <lvmorenoc@gmail.com>
2016-09-13 22:27:03 -0600

    S_OB_ERROR_HOW revised

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ErrorHowPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ErrorHowPM.java
@@ -1,22 +1,25 @@
 package seers.bugreppatterns.pattern.ob;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.bugreppatterns.utils.SentenceUtils;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
 public class ErrorHowPM extends ObservedBehaviorPatternMatcher {
-
-	final static String[] TOKENS = { "while", "when" };
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 
 		List<Token> tokens = sentence.getTokens();
 		List<Integer> errTerms = findMainTerms(tokens);
+
+		if (containsConditional(tokens) || containsVerbInPresent(tokens)) {
+			return 0;
+		}
+
 		for (Integer errTerm : errTerms) {
 			if (errTerm + 1 < tokens.size()) {
 				Token nextToken = tokens.get(errTerm + 1);
@@ -24,53 +27,11 @@ public class ErrorHowPM extends ObservedBehaviorPatternMatcher {
 				if (nextToken.getPos().equals("VBG")) {
 					return 1;
 
-					// cases like "occurred loading"
-				} else if (nextToken.getPos().equals("VBD")) {
-
-					if (errTerm + 2 < tokens.size()) {
-						Token nextToken2 = tokens.get(errTerm + 2);
-						if (nextToken2.getPos().equals("VBG")) {
-							return 1;
-						}
-					}
-					// cases like "is thrown"
-				} else if (nextToken.getPos().equals("VBZ") && nextToken.getLemma().equals("be")) {
-					if (errTerm + 2 < tokens.size()) {
-						Token nextToken2 = tokens.get(errTerm + 2);
-						if (nextToken2.getPos().equals("VBN")) {
-							return 1;
-						}
-					}
-
-					// cases like "error comes up"
-				} else if (nextToken.getPos().equals("VBZ") || nextToken.getPos().equals("VBP")) {
-
-					if (errTerm + 2 < tokens.size()) {
-						Token nextToken2 = tokens.get(errTerm + 2);
-						if (nextToken2.getPos().equals("RP")) {
-							return 1;
-						}
-					}
-					// cases like "error thrown"
-				} else if (nextToken.getPos().equals("VBN")) {
-					return 1;
 				} else if (nextToken.getGeneralPos().equals("NN")) {
 					// when the verb is wrongly detected as NN
 					if (nextToken.getLemma().endsWith("ing")) {
 						return 1;
-					} else {
-						// when there is compound error terms: "error page when"
-						if (errTerm + 2 < tokens.size()) {
-							Token nextToken2 = tokens.get(errTerm + 2);
-							if (Arrays.stream(TOKENS).anyMatch(t -> nextToken2.getLemma().contains(t))) {
-								return 1;
-							}
-						}
 					}
-					// when...
-				} else if (Arrays.stream(TOKENS).anyMatch(t -> nextToken.getLemma().contains(t)) && Arrays
-						.stream(new String[] { "NN", "JJ" }).anyMatch(t -> !nextToken.getGeneralPos().equals(t))) {
-					return 1;
 				}
 			}
 		}
@@ -89,5 +50,19 @@ public class ErrorHowPM extends ObservedBehaviorPatternMatcher {
 			}
 		}
 		return idxs;
+	}
+
+	private boolean containsVerbInPresent(List<Token> tokens) {
+		for (int i = 0; i < tokens.size(); i++) {
+			Token current = tokens.get(i);
+			if (current.getPos().equals("VBP") || current.getPos().equals("VBZ")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private boolean containsConditional(List<Token> tokens) {
+		return SentenceUtils.tokensContainAnyLemmaIn(tokens, CONDITIONAL_TERMS_2);
 	}
 }
