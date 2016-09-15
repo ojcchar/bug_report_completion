55d7c69806c6f13695098fecf4877adfbacbafaf
Oscar Chaparro <ojcch1@gmail.com>
2016-09-15 14:28:17 -0500

    CodeRefPM fixed

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/CodeRefPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/CodeRefPM.java
@@ -1,56 +1,187 @@
 package seers.bugreppatterns.pattern.sr;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import seers.bugreppatterns.pattern.StepsToReproducePatternMatcher;
+import seers.bugreppatterns.utils.JavaUtils;
 import seers.textanalyzer.TextProcessor;
 import seers.textanalyzer.entity.Sentence;
 import seers.textanalyzer.entity.Token;
 
 public class CodeRefPM extends StepsToReproducePatternMatcher {
 
-	final static String[] NOUNS_TERM = { "snippet", "code", "sample", "configuration", "statement", "script",
-			"html/ssi", "html/fbml", "html", "trace", "test.htm" };
-	final static String[] ADV_LOCATION = { "here", "below" };
-	final static String[] VERB_DEMO = { "provide", "enclose", "follow", "render", "attach" };
+	final static Set<String> NOUNS_TERM = JavaUtils.getSet("snippet", "code", "sample", "configuration", "statement",
+			"script", "html/ssi", "html/fbml", "html", "trace", "test.htm", "example", "project", "function", "method",
+			"class", "test", "configure", "build", "excerpt", "config", "program", "screen-shot", "screenshot", "video",
+			"log", "file", "line", "container", "attachment", "env", "build/install", "hql", "call", "apache",
+			"htpasswd", "installer", "command", "dockerfile", "tool", "test case", "example/test", "case", "parameter",
+			"model", "image");
+
+	final static String NOUN_TERMS_REGEX;
+	static {
+		StringBuffer buff = new StringBuffer("(");
+		NOUNS_TERM.forEach(nt -> {
+			buff.append(nt);
+			buff.append("|");
+		});
+		if (buff.length() != 0) {
+			buff.delete(buff.length() - 1, buff.length());
+		}
+		buff.append(")");
+		NOUN_TERMS_REGEX = buff.toString();
+	}
+
+	final static Set<String> LOCATION_TERMS = JavaUtils.getSet("here", "below", "above", "next");
+
+	final static Set<String> VERB_AS_ADJS = JavaUtils.getSet("provide", "enclose", "join", "attach", "upload",
+			"include");
+	final static Set<String> ADJS = JavaUtils.getSet("provided", "enclosed", "joined", "attached", "uploaded",
+			"included");
 
 	public int matchSentence(Sentence sentence) throws Exception {
 
-		String text = TextProcessor.getStringFromLemmas(sentence);
-		List<Token> tokens = sentence.getTokens();
-		boolean isNounTerm = false;
-		boolean isAdverb = false;
-		boolean isVerbAsAdjective = false;
-		boolean endsWith = false;
-		if (text.endsWith(":") || text.matches("(?s).*from :.*") || text.endsWith("e.g.")) {
-			endsWith = true;
-		}
-		if (text.matches("(?s).*(command line|live example|test case|file in).*")) {
-			isNounTerm = true;
-		}
-		for (int i = 0; i < tokens.size(); i++) {
-			Token token = tokens.get(i);
-			// exist a noun that refer to code such as code or snippet
-			if (Arrays.stream(NOUNS_TERM).anyMatch(t -> token.getLemma().equals(t))) {
-				if (token.getGeneralPos().equals("NN")) {
-					isNounTerm = true;
-				}
-			} else if (Arrays.stream(ADV_LOCATION).anyMatch(t -> token.getLemma().equals(t))
-					&& ((token.getGeneralPos().equals("RB")) || token.getGeneralPos().equals("IN"))) {
-				isAdverb = true;
-			} else if (Arrays.stream(VERB_DEMO).anyMatch(t -> token.getLemma().equals(t))
-					&& (token.getGeneralPos().equals("VB"))) {
-				isVerbAsAdjective = true;
-			}
+		// remove label: "created attachment 34535"
+		List<Token> tokensNoLabel = getTokensNoLabel(sentence.getTokens());
 
-		}
+		String text = TextProcessor.getStringFromLemmas(new Sentence(sentence.getId(), tokensNoLabel));
 
-		if (isNounTerm && (isAdverb || isVerbAsAdjective || endsWith)) {
+		// -----------------------------------
+
+		// cases: this/here is the snippet
+		String regex = "(?s).*(this|here) be [a-zA-Z]+ (\\p{Punct}+ )?" + NOUN_TERMS_REGEX + ".*";
+		if (text.matches(regex)) {
 			return 1;
 		}
+
+		// -----------------------------------
+
+		int nounIdx = -1;
+		boolean isLocationTerm = false;
+		boolean isAdjective = false;
+		boolean endsWith = false;
+		boolean nextColon = false;
+		boolean isFollowingNoun = false;
+
+		// check for some common sentence ends: ":", "e.g.", "like:", etc
+		if (text.endsWith(":") || text.endsWith("e.g.")
+				|| text.matches("(?s).*(,) (for example|as|like|like this|here) :$")) {
+			endsWith = true;
+		}
+
+		for (int i = 0; i < tokensNoLabel.size(); i++) {
+			Token token = tokensNoLabel.get(i);
+
+			// there is a noun that refer to code, files, etc.
+			if (NOUNS_TERM.stream().anyMatch(t -> token.getLemma().equalsIgnoreCase(t))) {
+				if (token.getGeneralPos().equals("NN")) {
+					nounIdx = i;
+					if (i + 1 < tokensNoLabel.size()) {
+
+						// case: "code:"
+						if (tokensNoLabel.get(i + 1).getLemma().equals(":")) {
+							nextColon = true;
+						} else {
+							// case "code is"
+							if (i + 2 < tokensNoLabel.size()) {
+								if (tokensNoLabel.get(i + 1).getGeneralPos().equals("VB")
+										&& tokensNoLabel.get(i + 1).getLemma().equals("be")
+										&& tokensNoLabel.get(i + 2).getLemma().equals(":")) {
+									nextColon = true;
+								}
+							}
+						}
+					}
+				}
+
+				// check for location terms
+			} else if (LOCATION_TERMS.stream().anyMatch(t -> token.getLemma().equalsIgnoreCase(t))
+					&& ((token.getGeneralPos().equals("RB")) || token.getGeneralPos().equals("IN")
+							|| token.getGeneralPos().equals("JJ"))) {
+				isLocationTerm = true;
+
+				// check for adjectives expressing attachment
+			} else if ((VERB_AS_ADJS.stream().anyMatch(t -> token.getLemma().equalsIgnoreCase(t))
+					&& token.getPos().equals("VBN"))
+					|| (token.getLemma().equals("follow") && token.getPos().equals("VBG"))
+					|| (ADJS.stream().anyMatch(t -> token.getLemma().equalsIgnoreCase(t))
+							&& token.getGeneralPos().equals("JJ"))) {
+				isAdjective = true;
+
+				// check for "following"
+			} else if (token.getLemma().equals("following")) {
+				isFollowingNoun = true;
+			}
+
+			// if there is at least a code term and (location term, adjective,
+			// it's followed by a colon, "following")
+			if (nounIdx != -1 && (isLocationTerm || isAdjective || endsWith || nextColon || isFollowingNoun)) {
+				return 1;
+			}
+		}
+
+		// -------------------------
+
+		// check for just the noun, no verb
+		// case: "Excel file with macro
+		if (nounIdx != -1 && !tokensNoLabel.stream().anyMatch(t -> t.getGeneralPos().equals("VB"))) {
+			return 1;
+
+			// case allowed: "validated file"
+		} else if (nounIdx != -1 && nounIdx - 1 >= 0) {
+			if (tokensNoLabel.get(nounIdx - 1).getPos().equals("VBN")) {
+				boolean otherTokensOk = true;
+				for (int i = 0; i < tokensNoLabel.size(); i++) {
+					if (nounIdx != i && nounIdx - 1 != i) {
+						Token t = tokensNoLabel.get(i);
+						if (t.getGeneralPos().equals("VB")) {
+							otherTokensOk = false;
+							break;
+						}
+					}
+				}
+				if (otherTokensOk) {
+					return 1;
+				}
+			}
+		}
+
+		// -------------------------------
+
+		// cases: ... the code shows: ...
+		if (nounIdx != -1 && text.matches(".* " + tokensNoLabel.get(nounIdx).getLemma() + " .* : .*")) {
+			return 1;
+		}
+
+		// -------------------------
+
 		return 0;
 
+	}
+
+	private List<Token> getTokensNoLabel(List<Token> tokens) {
+		List<Token> tokensNoLabel = new ArrayList<>();
+
+		// remove label
+		for (int i = 0; i < tokens.size();) {
+
+			Token token = tokens.get(i);
+			if (token.getLemma().equals("create") && token.getPos().equals("VBN")) {
+				if (i + 2 < tokens.size()) {
+					if (tokens.get(i + 1).getLemma().equals("attachment")
+							&& tokens.get(i + 2).getLemma().matches("\\d+")) {
+						i += 3;
+						continue;
+					}
+
+				}
+			}
+
+			tokensNoLabel.add(token);
+			i++;
+		}
+		return tokensNoLabel;
 	}
 
 }
--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/CodeRefPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/CodeRefPMTest.java
@@ -1,10 +1,30 @@
 package seers.bugreppatterns.pattern.sr;
 
+import org.junit.Test;
+
 import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
 
 public class CodeRefPMTest extends BaseTest {
 
 	public CodeRefPMTest() {
 		pm = new CodeRefPM();
 	}
+
+	@Test
+	public void testPositives() throws Exception {
+
+		String[] txts = { "Created attachment 56395\nInvalidated file" };
+		TestUtils.testSentences(txts, pm, 1);
+
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+
+		String[] txts = { "Created attachment 56395\n", "My code is nice", "Currently there is code in `/utils/daemon.",
+				"I was able to checkout the code install go and run make in the root directory to make the build a few days ago." };
+		TestUtils.testSentences(txts, pm, 0);
+	}
+
 }
