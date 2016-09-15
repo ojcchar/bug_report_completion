0ba00c21092ffa0b11e52b908e4ed2461680a414
Laura Moreno <lvmorenoc@gmail.com>
2016-09-15 11:34:08 -0600

    Some tests after merging S_OB_BELIEVE_WHILE_IN_REALITY with conditional patterns

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/PositiveConditionalPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/PositiveConditionalPMTest.java
@@ -1,10 +1,30 @@
 package seers.bugreppatterns.pattern.ob;
 
-import seers.bugreppatterns.main.BaseTest;
+import org.junit.Test;
 
-public class PositiveConditionalPMTest extends BaseTest{
+import seers.bugreppatterns.main.BaseTest;
+import seers.bugreppatterns.pattern.utils.TestUtils;
+
+public class PositiveConditionalPMTest extends BaseTest {
 
 	public PositiveConditionalPMTest() {
 		pm = new PositiveConditionalPM();
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+		String[] sentences = {
+
+		};
+
+		TestUtils.testSentences(sentences, pm, 0);
+	}
+
+	@Test
+	public void testPositive() throws Exception {
+		String[] sentences = {
+				"With a certain configuration of Virtual hosts with SSL Engine enabled, Apache beliefs the client is connecting to server port 80 with protocol HTTP, while in reality the client is connected to server port 443 with protocol HTTPS",
+				};
+		TestUtils.testSentences(sentences, pm, 1);
 	}
 }
