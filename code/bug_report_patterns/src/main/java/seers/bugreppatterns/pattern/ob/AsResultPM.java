748b33b8d802a54529a4d87195adf80b877378e4
Laura Moreno <lvmorenoc@gmail.com>
2016-09-15 15:14:02 -0600

    Revised S_OB_AS_RESULT after merge with S_OB_CONSEQUENCE

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/AsResultPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/AsResultPM.java
@@ -8,10 +8,9 @@ public class AsResultPM extends ObservedBehaviorPatternMatcher {
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-
 		String txt = TextProcessor.getStringFromLemmas(sentence);
-		if ((txt.toLowerCase().matches("^.*[^A-Za-z]?as result[^A-Za-z]?.*")
-				|| txt.toLowerCase().matches("^.*[^A-Za-z]?as a result[^A-Za-z]?.*"))
+		if ((txt.toLowerCase().matches("^.*[^a-z]?as (a )?result[^a-z]?.*")
+				|| txt.toLowerCase().matches("^.*[^a-z]?the (consequence|implication) be[^a-z].*"))
 				&& !isEBModal(sentence.getTokens())) {
 			return 1;
 		}
