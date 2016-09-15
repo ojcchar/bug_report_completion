6422389f49a174d4285cfbde5bc0c9dcfdc97698
Laura Moreno <lvmorenoc@gmail.com>
2016-09-15 12:34:14 -0600

    Revised S_OB_ACTION_SUBJECT pattern

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/GerundSubjectPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/GerundSubjectPM.java
@@ -11,24 +11,40 @@ import seers.textanalyzer.entity.Token;
 
 public class GerundSubjectPM extends ObservedBehaviorPatternMatcher {
 
-	final private static Set<String> VERBS_AS_NOUNS = JavaUtils.getSet("copy", "drag", "insert" );
+	final private static Set<String> VERBS_AS_NOUNS = JavaUtils.getSet("copy", "drag", "insert");
+
+	private static final Set<String> PUNCTUATION = JavaUtils.getSet(":", ";", ".");
 
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-		List<Token> tokens = sentence.getTokens();
-		Token first = tokens.get(0);
-		if (first.getPos().equals("VBG") || (first.getLemma().endsWith("ing") && first.getGeneralPos().equals("NN"))) {
-			for (int i = 1; i < tokens.size(); i++) {
-				Token token = tokens.get(i);
-				if (token.getGeneralPos().equals("VB")) {
-					return 1;
-				} else if (token.getGeneralPos().equals("NN")
-						&& SentenceUtils.lemmasContainToken(VERBS_AS_NOUNS, token)) {
-					return 1;
+
+		List<Sentence> subSentences = SentenceUtils.findSubSentences(sentence, findPunctuation(sentence.getTokens()));
+
+		for (Sentence subSentence : subSentences) {
+			List<Token> tokens = subSentence.getTokens();
+			Token first = tokens.get(0);
+			if (first.getPos().equals("VBG")
+					|| (first.getWord().toLowerCase().endsWith("ing") && first.getGeneralPos().equals("NN"))) {
+				for (int i = 1; i < tokens.size(); i++) {
+					Token token = tokens.get(i);
+					if (token.getGeneralPos().equals("VB")) {
+						return 1;
+					} else if (token.getGeneralPos().equals("NN")
+							&& SentenceUtils.lemmasContainToken(VERBS_AS_NOUNS, token)) {
+						return 1;
+					}
 				}
 			}
 		}
 		return 0;
+	}
+
+	private List<Integer> findPunctuation(List<Token> tokens) {
+		List<Integer> symbols = SentenceUtils.findLemmasInTokens(PUNCTUATION, tokens);
+		if (symbols.size() - 1 >= 0 && symbols.get(symbols.size() - 1) == tokens.size() - 1) {
+			return symbols.subList(0, symbols.size() - 1);
+		}
+		return symbols;
 	}
 
 }
