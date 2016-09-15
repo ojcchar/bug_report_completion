905e2c8acefeb5874abc79e57515e5169a6d23f9
Laura Moreno <lvmorenoc@gmail.com>
2016-09-15 11:23:22 -0600

    Some tests after merging S_OB_NOTHING_END with S_OB_VERB_NO

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/VerbNoPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/VerbNoPMTest.java
@@ -20,7 +20,11 @@ public class VerbNoPMTest extends BaseTest {
 
 	@Test
 	public void testPositives() throws Exception {
-		String[] n = { "it returns with no error", "it returns with no name", "When we runs the script from an existing dos window the command prompt returns with no output." };
+		String[] n = { "it returns with no error", "it returns with no name",
+				"When we runs the script from an existing dos window the command prompt returns with no output.",
+				"Double-clicking the background in TabCandy currently does nothing.",
+				"As you can see, there's no language set and spellcheck did nothing.",
+				"Media: Tapping refresh with no internet connection does nothing." };
 
 		TestUtils.testSentences(n, pm, 1);
 	}
