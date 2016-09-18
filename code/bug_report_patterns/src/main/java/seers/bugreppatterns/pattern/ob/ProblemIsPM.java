f53d34318e569bf7594bf7fdfda8d14272985e16
Laura Moreno <lvmorenoc@gmail.com>
2016-09-18 10:45:47 -0600

    Revised S_OB_PROBLEM_IS. All its tests pass.

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ProblemIsPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ProblemIsPM.java
@@ -1,22 +1,47 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.List;
+import java.util.Set;
+
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
-import seers.bugreppatterns.pattern.PatternMatcher;
-import seers.textanalyzer.TextProcessor;
+import seers.bugreppatterns.utils.JavaUtils;
+import seers.bugreppatterns.utils.SentenceUtils;
 import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
 
 public class ProblemIsPM extends ObservedBehaviorPatternMatcher {
 
+	private final static Set<String> PROBLEM_TERMS = JavaUtils.getSet("problem", "issue");
+
+	private final static Set<String> PREV_PROB_POS = JavaUtils.getSet("PRP", "DT", "CD", "JJ");
+
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
-		String text = TextProcessor.getStringFromLemmas(sentence);
-		if (text.startsWith("the problem be") || text.startsWith("the issue be") || text.startsWith("my problem be")) {
-			PatternMatcher pm = new ProblemInPM();
-			int match = pm.matchSentence(sentence);
-			if (match == 0) {
+
+		List<Token> tokens = sentence.getTokens();
+
+		int i = 0;
+		Token current = tokens.get(i);
+
+		while (PREV_PROB_POS.contains(current.getGeneralPos())) {
+			i++;
+			current = tokens.get(i);
+		}
+		
+		if (SentenceUtils.lemmasContainToken(PROBLEM_TERMS, current) && current.getGeneralPos().equals("NN")) {
+			i++;
+			current = tokens.get(i);
+
+			while (!current.getGeneralPos().equals("VB")) {
+				i++;
+				current = tokens.get(i);
+			}
+
+			if (current.getLemma().equals("be") && (current.getPos().equals("VBP") || current.getPos().equals("VBZ"))) {
 				return 1;
 			}
 		}
+
 		return 0;
 	}
 
