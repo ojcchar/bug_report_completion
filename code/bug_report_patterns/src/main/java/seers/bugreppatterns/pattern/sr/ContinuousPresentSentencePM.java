ad6073c1f49dcc4401dda89afba29b7b24af2c74
juan <jmflorezff@gmail.com>
2016-09-20 11:53:24 -0500

    Implemented S_SR_CONTINOUS_PRESENT and S_SR_PURPOSE_ACTION.

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/sr/ContinuousPresentSentencePM.java
@@ -0,0 +1,16 @@
+package seers.bugreppatterns.pattern.sr;
+
+import seers.bugreppatterns.pattern.StepsToReproducePatternMatcher;
+import seers.textanalyzer.entity.Sentence;
+
+/**
+ * Created by juan on 9/20/16.
+ */
+public class ContinuousPresentSentencePM extends StepsToReproducePatternMatcher {
+    @Override
+    public int matchSentence(Sentence sentence) throws Exception {
+        return sentence.getTokens().stream()
+                .filter(t -> t.getGeneralPos().equals("VB"))
+                .allMatch(t -> t.getPos().equals("VBG") || t.getLemma().equals("be")) ? 1 : 0;
+    }
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/sr/ContinuousPresentSentencePMTest.java
@@ -0,0 +1,12 @@
+package seers.bugreppatterns.pattern.sr;
+
+import seers.bugreppatterns.main.BaseTest;
+
+/**
+ * Created by juan on 9/20/16.
+ */
+public class ContinuousPresentSentencePMTest extends BaseTest {
+    {
+        pm = new ContinuousPresentSentencePM();
+    }
+}
