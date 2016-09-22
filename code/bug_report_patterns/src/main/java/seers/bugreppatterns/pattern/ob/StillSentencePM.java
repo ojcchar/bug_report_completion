b5cbe6892ecd04fe316f0bd2c876055afe83f8b6
Laura Moreno <lvmorenoc@gmail.com>
2016-09-21 18:55:32 -0600

    Revised S_OB_STILL. All its tests pass

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/StillSentencePM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/StillSentencePM.java
@@ -9,7 +9,7 @@ public class StillSentencePM extends ObservedBehaviorPatternMatcher {
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 		String txt = TextProcessor.getStringFromLemmas(sentence);
-		if (txt.matches(".+still.+")) {
+		if (txt.matches(".*[^a-z]?still .+") || txt.matches(".+ still[^a-z]?.*")) {
 			return 1;
 		}
 		return 0;
