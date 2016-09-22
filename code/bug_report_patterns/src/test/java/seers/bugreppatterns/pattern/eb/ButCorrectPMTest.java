cf061467a281c83120e7388a2805686ee70fa9d1
juan <jmflorezff@gmail.com>
2016-09-22 11:32:16 -0500

    Implemented S_EB_BUT_CORRECT.

---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/eb/ButCorrectPMTest.java
@@ -0,0 +1,14 @@
+package seers.bugreppatterns.pattern.eb;
+
+import seers.bugreppatterns.main.BaseTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by juan on 9/22/16.
+ */
+public class ButCorrectPMTest extends BaseTest {
+    {
+        pm = new ButCorrectPM();
+    }
+}
