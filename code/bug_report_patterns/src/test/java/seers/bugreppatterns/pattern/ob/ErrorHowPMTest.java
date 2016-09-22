3bd4e96bfafd61b4ee109cc7df617a20df9a4dbc
Oscar Chaparro <ojcch1@gmail.com>
2016-09-22 17:06:42 -0500

    All tests pass for ErrorHow

--- code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ErrorHowPMTest.java
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ErrorHowPMTest.java
@@ -6,21 +6,28 @@ import seers.bugreppatterns.main.BaseTest;
 import seers.bugreppatterns.pattern.utils.TestUtils;
 
 public class ErrorHowPMTest extends BaseTest {
-    public ErrorHowPMTest() {
-        pm = new ErrorHowPM();
-    }
 
-    @Test
-    public void testNegative() throws Exception {
-        String[] negs = {"this error comes up",
-                "Actual result:\nException is thrown out.",
-                "The issues causing these errors to be reported are also causing all tests in " +
-                        "TestPluginMainValidator to fail.",
-                "A javascript error shows up_ and the whole control is gone.",
-                "The thread safety issue is caused by having multiple threads that share a state " +
-                        "in a non-thread safe {{HashSet}}.",
-                "The following error is reported in the error log:"};
+	public ErrorHowPMTest() {
+		pm = new ErrorHowPM();
+	}
 
-        TestUtils.testSentences(negs, pm, 0);
-    }
+	@Test
+	public void testPositive() throws Exception {
+		String[] txt = {};
+
+		TestUtils.testSentences(txt, pm, 1);
+	}
+
+	@Test
+	public void testNegative() throws Exception {
+		String[] negs = { "this error comes up", "Actual result:\nException is thrown out.",
+				"The issues causing these errors to be reported are also causing all tests in "
+						+ "TestPluginMainValidator to fail.",
+				"A javascript error shows up_ and the whole control is gone.",
+				"The thread safety issue is caused by having multiple threads that share a state "
+						+ "in a non-thread safe {{HashSet}}.",
+				"The following error is reported in the error log:", };
+
+		TestUtils.testSentences(negs, pm, 0);
+	}
 }
