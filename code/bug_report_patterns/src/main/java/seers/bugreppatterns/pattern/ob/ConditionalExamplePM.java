41a5b08b04c9598f487a321a5a9eb4191465f83c
juan <jmflorezff@gmail.com>
2016-09-22 10:02:19 -0500

    Implemented P_OB_COND_EXAMPLE.

---
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/ConditionalExamplePM.java
@@ -0,0 +1,51 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.entity.Paragraph;
+import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.bugreppatterns.utils.JavaUtils;
+import seers.bugreppatterns.utils.SentenceUtils;
+import seers.bugreppatterns.utils.SimpleTenseChecker;
+import seers.textanalyzer.entity.Sentence;
+import seers.textanalyzer.entity.Token;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Matcher for P_OB_COND_EXAMPLE.
+ */
+public class ConditionalExamplePM extends ObservedBehaviorPatternMatcher {
+    @Override
+    public int matchSentence(Sentence sentence) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public int matchParagraph(Paragraph paragraph) throws Exception {
+        List<Sentence> sentences = paragraph.getSentences();
+        Sentence firstSentence = sentences.get(0);
+        List<Token> tokens = firstSentence.getTokens();
+        List<Integer> conditionalIndexes =
+                SentenceUtils.findLemmasInTokens(ConditionalNegativePM.CONDITIONAL_TERMS,
+                        tokens);
+
+        if (conditionalIndexes.get(0) == 0 && tokens.size() >= 4) {
+            int tokensSize = tokens.size();
+            List<Token> firstSentenceEnding = tokens.subList(tokensSize - 3, tokensSize - 1);
+
+            if (firstSentenceEnding.get(0).getLemma().equals("like") &&
+                    firstSentenceEnding.get(1).getLemma().equals("this")) {
+                SimpleTenseChecker simplePresentChecker =
+                        new SimpleTenseChecker(JavaUtils.getSet("VBP", "VBZ"),
+                                Collections.emptySet());
+
+                if (sentences.subList(1, sentences.size()).stream()
+                        .anyMatch(s -> simplePresentChecker.countNumClauses(s) > 0)) {
+                    return 1;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ code/bug_report_patterns/src/test/java/seers/bugreppatterns/pattern/ob/ConditionalExamplePMTest.java
@@ -0,0 +1,14 @@
+package seers.bugreppatterns.pattern.ob;
+
+import seers.bugreppatterns.main.BaseTest;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by juan on 9/22/16.
+ */
+public class ConditionalExamplePMTest extends BaseTest {
+    {
+        pm = new ConditionalExamplePM();
+    }
+}
