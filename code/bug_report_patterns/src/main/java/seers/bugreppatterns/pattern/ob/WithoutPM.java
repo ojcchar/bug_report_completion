783466f8d4e9e4d697c61c090b785ec3fadd55a7
Oscar Chaparro <ojcch1@gmail.com>
2016-10-02 22:45:09 -0500

    Data updated, WithouthPM fixed

--- code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/WithoutPM.java
+++ code/bug_report_patterns/src/main/java/seers/bugreppatterns/pattern/ob/WithoutPM.java
@@ -1,6 +1,9 @@
 package seers.bugreppatterns.pattern.ob;
 
+import java.util.List;
+
 import seers.bugreppatterns.pattern.ObservedBehaviorPatternMatcher;
+import seers.bugreppatterns.utils.SentenceUtils;
 import seers.textanalyzer.TextProcessor;
 import seers.textanalyzer.entity.Sentence;
 
@@ -9,9 +12,13 @@ public class WithoutPM extends ObservedBehaviorPatternMatcher {
 	@Override
 	public int matchSentence(Sentence sentence) throws Exception {
 
-		String txt = TextProcessor.getStringFromLemmas(sentence);
-		if ((txt.contains("without") || txt.contains("with no ")) && !isEBModal(sentence.getTokens())) {
-			return 1;
+		List<Sentence> clauses = SentenceUtils.extractClauses(sentence);
+		
+		for (Sentence clause : clauses) {
+			String txt = TextProcessor.getStringFromLemmas(clause);
+			if ((txt.contains("without") || txt.contains("with no ")) && !isEBModal(clause.getTokens())) {
+				return 1;
+			}
 		}
 		return 0;
 	}
