e909db6a5f1a77c4235c871bf673e8c564845e1d
Oscar Chaparro <ojcch1@gmail.com>
2017-03-09 22:36:03 -0600

    test case for SVM based classifier

--- code/bug_report_classifier/src/main/java/seers/bugrepclassifier/svmbased/SVMBugReportClassifier.java
+++ code/bug_report_classifier/src/main/java/seers/bugrepclassifier/svmbased/SVMBugReportClassifier.java
@@ -1,6 +1,7 @@
 package seers.bugrepclassifier.svmbased;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,46 +26,57 @@ public class SVMBugReportClassifier extends BugReportClassifier {
 		// generate features
 
 		BugReport report = new BugReport(bugDescription);
-		BugReportFea fea = new BugReportFea(false, "svm_model/exp/BugReportFea_exp_all");
+		String model = "svm_model/exp/exp-modelall-0.005-b-pngram-light";
+		String featuresFile = "svm_model/exp/BugReportFea_exp_all";
+		String suffix = "eb";
+		String ebClass = classifyInstance(model, featuresFile, suffix, bugId, report);
 
+		String srModel = "svm_model/step/step-modelall-0.01-b-pngram-light";
+		String srFeaturesFile = "svm_model/step/BugReportFea_step_all";
+		String srSuffix = "sr";
+		String srClass = classifyInstance(srModel, srFeaturesFile, srSuffix, bugId, report);
+
+		Labels labels = new Labels("", ebClass, srClass);
+		PredictionOutput output = new PredictionOutput(labels, null);
+
+		return output;
+	}
+
+	private String classifyInstance(String model, String featuresFile, String suffix, String bugId, BugReport report)
+			throws IOException, InterruptedException {
+
+		BugReportFea fea = new BugReportFea(false, featuresFile);
 		fea.configure(report);
 		String feaStr = fea.getSVMFormatString();
 
 		ArrayList<String> testline = new ArrayList<String>();
 		testline.add("+1 " + feaStr);
 
-		String svmFile = "output/" + bugId + ".svm";
+		String svmFile = "output/" + bugId + ".svm." + suffix;
 		Common.outputLines(testline, svmFile, false);
 
-		String outFile = "output/" + bugId + ".out";
+		String outFile = "output/" + bugId + ".out." + suffix;
 		// classify
 
-		try {
-			String line;
-			String command = "svm_light/v6.01/svm_light_windows/svm_classify.exe -f 0 " + svmFile
-					+ " svm_model/exp/exp-modelall-0.005-b-pngram-light " + outFile;
-			System.out.println(command);
-			Process p = Runtime.getRuntime().exec(command);
-//			BufferedReader input = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//			while ((line = input.readLine()) != null) {
-//				System.out.println(line);
-//			}
-//			input.close();
-			p.waitFor();
-		} catch (Exception err) {
-			err.printStackTrace();
-		}
+		String command = "svm_light/v6.01/svm_light_windows/svm_classify.exe -f 0 " + svmFile + " " + model + " "
+				+ outFile;
+//		System.out.println(command);
+		Process p = Runtime.getRuntime().exec(command);
+		// String line;
+		// BufferedReader input = new BufferedReader(new
+		// InputStreamReader(p.getInputStream()));
+		// while ((line = input.readLine()) != null) {
+		// System.out.println(line);
+		// }
+		// input.close();
+		p.waitFor();
 
 		// read output
 		List<String> lines = FileUtils.readLines(new File(outFile));
 		String line = lines.get(0);
-
 		String cls = line.split(" ")[0].split(":")[1];
 
-		Labels labels = new Labels("", cls.equals("+1") ? "x" : "", "");
-		PredictionOutput output = new PredictionOutput(labels, null);
-
-		return output;
+		return cls.equals("+1") ? "x" : "";
 	}
 
 }
--- code/bug_report_classifier/src/main/java/seers/bugrepclassifier/svmbased/model/BugReport.java
+++ code/bug_report_classifier/src/main/java/seers/bugrepclassifier/svmbased/model/BugReport.java
@@ -33,30 +33,36 @@ public class BugReport {
 	public boolean steps_2_repr;
 	public ArrayList<String> paragraphs;
 	public HashMap<String, ArrayList<String>> paragraphMap;
-	public HashMap<String,String> sentenceMap;
+	public HashMap<String, String> sentenceMap;
 	public ArrayList<ParseResult> parseResults;
-	public HashMap<String,ParseResult> parseResultsMap;
+	public HashMap<String, ParseResult> parseResultsMap;
 
 	public HashMap<Integer, int[]> positionMap;
 
-	
-	public BugReport(String fileID,String system){
+	static StanfordCoreNLP pipeline;
+	static {
+		Properties props = new Properties();
+		props.setProperty("annotators", "tokenize, ssplit");
+		pipeline = new StanfordCoreNLP(props);
+	}
+
+	public BugReport(String fileID, String system) {
 		System.setProperty("file.encoding", "UTF-8");
 		this.paragraphMap = new HashMap<String, ArrayList<String>>();
-		this.sentenceMap = new HashMap<String,String>();
+		this.sentenceMap = new HashMap<String, String>();
 		this.fileID = fileID;
 		this.system = system;
 		this.xmlLine = Common.getLine(fileID);
 		this.title = "";
-		this.reportID="";
+		this.reportID = "";
 		this.description = "";
 		this.readSource();
 		this.preprocessing();
 	}
-	
+
 	public BugReport(String bugDescription) {
 		this.paragraphMap = new HashMap<String, ArrayList<String>>();
-		this.sentenceMap = new HashMap<String,String>();
+		this.sentenceMap = new HashMap<String, String>();
 		this.description = bugDescription;
 		this.readSource2();
 		this.preprocessing();
@@ -65,9 +71,9 @@ public class BugReport {
 	private void readSource2() {
 		this.paragraphs = new ArrayList<String>();
 		String[] temp = this.description.split("\n\n");
-		for(int i = 0;i<temp.length;i++){
+		for (int i = 0; i < temp.length; i++) {
 			String s = temp[i];
-			if(s.isEmpty()){
+			if (s.isEmpty()) {
 				continue;
 			}
 			paragraphs.add(s);
@@ -76,8 +82,9 @@ public class BugReport {
 
 	public void readSource() {
 		this.paragraphs = new ArrayList<String>();
-		this.xmlLine = this.xmlLine.replace("<description>", "<description><![CDATA[").replace("</description>", "]]></description>")
-				.replace("<title>", "<title><![CDATA[").replace("</title>", "]]></title>");
+		this.xmlLine = this.xmlLine.replace("<description>", "<description><![CDATA[")
+				.replace("</description>", "]]></description>").replace("<title>", "<title><![CDATA[")
+				.replace("</title>", "]]></title>");
 		InputStream inputStream;
 		try {
 			inputStream = new ByteArrayInputStream(this.xmlLine.getBytes());
@@ -87,156 +94,161 @@ public class BugReport {
 			BugReportReader reader = new BugReportReader();
 			sp.parse(new InputSource(inputStream), reader);
 			this.reportID += reader.getID();
-			this.description += reader.getDescription().replaceAll("````", "\"\"").replaceAll("```", "\"'").
-					replaceAll("``","\"").replaceAll("`","'").replaceAll("’", "'")
-					.replaceAll("”", "\"").replaceAll("“", "\"").replaceAll("—", "--");
+			this.description += reader.getDescription().replaceAll("````", "\"\"").replaceAll("```", "\"'")
+					.replaceAll("``", "\"").replaceAll("`", "'").replaceAll("’", "'").replaceAll("”", "\"")
+					.replaceAll("“", "\"").replaceAll("—", "--");
 			this.title += reader.getTitle();
-			
-			//System.out.println(description);
+
+			// System.out.println(description);
 			String[] temp = this.description.split("\n\n");
-			for(int i = 0;i<temp.length;i++){
+			for (int i = 0; i < temp.length; i++) {
 				String s = temp[i];
-				if(s.isEmpty()){
-					//Common.pause("empty paragraph");
+				if (s.isEmpty()) {
+					// Common.pause("empty paragraph");
 					continue;
 				}
 				paragraphs.add(s);
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void preprocessing(){
+
+	public void preprocessing() {
 
 		this.parseResults = new ArrayList<ParseResult>();
-		
-		Properties props = new Properties();
-	    props.setProperty("annotators", "tokenize, ssplit");
-	    StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
-    
-	    for(int pid = 0;pid<this.paragraphs.size();pid++){
-	    	//System.out.println(i+" "+report.paragraphs.get(i).length());
-	    	
-	    	String paragraph = this.paragraphs.get(pid);
-	    	String[] ss = paragraph.split("\n");
-	    	
-	    	//flag used to indicate whether we split paragraph by row, to check if there exists some special case
-	    	boolean flag = false;
-	    	
-	    	if(paragraph.toLowerCase().startsWith("steps to reproduce")){
-	    		flag = true;
-	    	}else{
-	    		for(String s:ss){
-	    			//pattern 1.) 1. 1) - 
-	    			if(s.matches("(\\W*)\\d((\\.\\))|\\.|\\)|,|-)(.*)")||s.matches("(\\W*)(-|\\*)(.*)")){
-	    				flag = true;
-	    				break;
-	    			}
-	    		}
-	    	}
-	    	
-	    	boolean errorflag = false;
-	    	for(String s:ss){
-    			if(s.matches("(\\W*)(at )(.*)")){
-    				errorflag = true;
-    				break;
-    			}
-    		}
-	    	
-	    	
-	    	//System.out.println(flag);
-	    	
-	    	ArrayList<String> parseSentences = new ArrayList<String>();
-	    	//if paragraph contains specical patterns, corenlp will process line by line. 
-	    	//Otherwise, process the paragraph directly
-	    	if(flag==true){
-	    		for(String s:ss){
-	    			parseSentences.add(s);
-	    		}
-	    	}else{
-    			parseSentences.add(paragraph);
-    		}
-	    	
-	    	ArrayList<String> splittedSentence = new ArrayList<String>();
-	    	
-	    	//paragraph contains error code such as:at java.lang.Object.wait(Native Method) will not be processed by corenlp
-//	    	if((errorflag==true || report.paragraphs.get(pid).length()>700) && flag==false){
-//	    		for(String s:ss){
-//	    			splittedSentence.add(s);
-//	    		}
-//	    		this.paragraphMap.put(String.valueOf(pid),splittedSentence);
-//	    		//splitfile.add(splittedSentence);
-//	    		//Common.pause(" ");
-//	    		continue;
-//	    	}
-	    	
-	    	
-	    	for(int id = 0;id<parseSentences.size();id++){
-	    		String parseSentence = parseSentences.get(id);
-//	    		if(parseSentence.startsWith("java.")||parseSentence.startsWith("	at ")||parseSentence.startsWith("\"") ||parseSentence.startsWith("org.apache")){//parseSentence.matches("(\\W*)(at )(\\w+\\.){3,}(.*)")){
-//	    			System.out.println("NOT processing: "+parseSentence);
-//	    			splittedSentence.add(parseSentence);
-//	    			if(sid==parseSentences.size()-1){
-//	    				//splitfile.add(splittedSentence);
-//	    				this.paragraphMap.put(String.valueOf(pid),splittedSentence);
-//	    			}
-//	    			continue;
-//	    		}
-	    		
-	    		if(parseSentence.matches("(\\W*)\\d((\\.\\))|\\.|\\)|,|-)(.*)")){
-	    			parseSentence = parseSentence.replaceFirst("((\\.\\))|\\.|\\)|,|-)\\s*"," ");
-	    		//	System.out.println(parseSentence);
-	    		}
-	    		//System.out.println(parseSentence);
-		    	Annotation document = new Annotation(parseSentence);
-			    
+
+		for (int pid = 0; pid < this.paragraphs.size(); pid++) {
+			// System.out.println(i+" "+report.paragraphs.get(i).length());
+
+			String paragraph = this.paragraphs.get(pid);
+			String[] ss = paragraph.split("\n");
+
+			// flag used to indicate whether we split paragraph by row, to check
+			// if there exists some special case
+			boolean flag = false;
+
+			if (paragraph.toLowerCase().startsWith("steps to reproduce")) {
+				flag = true;
+			} else {
+				for (String s : ss) {
+					// pattern 1.) 1. 1) -
+					if (s.matches("(\\W*)\\d((\\.\\))|\\.|\\)|,|-)(.*)") || s.matches("(\\W*)(-|\\*)(.*)")) {
+						flag = true;
+						break;
+					}
+				}
+			}
+
+			boolean errorflag = false;
+			for (String s : ss) {
+				if (s.matches("(\\W*)(at )(.*)")) {
+					errorflag = true;
+					break;
+				}
+			}
+
+			// System.out.println(flag);
+
+			ArrayList<String> parseSentences = new ArrayList<String>();
+			// if paragraph contains specical patterns, corenlp will process
+			// line by line.
+			// Otherwise, process the paragraph directly
+			if (flag == true) {
+				for (String s : ss) {
+					parseSentences.add(s);
+				}
+			} else {
+				parseSentences.add(paragraph);
+			}
+
+			ArrayList<String> splittedSentence = new ArrayList<String>();
+
+			// paragraph contains error code such as:at
+			// java.lang.Object.wait(Native Method) will not be processed by
+			// corenlp
+			// if((errorflag==true || report.paragraphs.get(pid).length()>700)
+			// && flag==false){
+			// for(String s:ss){
+			// splittedSentence.add(s);
+			// }
+			// this.paragraphMap.put(String.valueOf(pid),splittedSentence);
+			// //splitfile.add(splittedSentence);
+			// //Common.pause(" ");
+			// continue;
+			// }
+
+			for (int id = 0; id < parseSentences.size(); id++) {
+				String parseSentence = parseSentences.get(id);
+				// if(parseSentence.startsWith("java.")||parseSentence.startsWith("
+				// at ")||parseSentence.startsWith("\"")
+				// ||parseSentence.startsWith("org.apache")){//parseSentence.matches("(\\W*)(at
+				// )(\\w+\\.){3,}(.*)")){
+				// System.out.println("NOT processing: "+parseSentence);
+				// splittedSentence.add(parseSentence);
+				// if(sid==parseSentences.size()-1){
+				// //splitfile.add(splittedSentence);
+				// this.paragraphMap.put(String.valueOf(pid),splittedSentence);
+				// }
+				// continue;
+				// }
+
+				if (parseSentence.matches("(\\W*)\\d((\\.\\))|\\.|\\)|,|-)(.*)")) {
+					parseSentence = parseSentence.replaceFirst("((\\.\\))|\\.|\\)|,|-)\\s*", " ");
+					// System.out.println(parseSentence);
+				}
+				// System.out.println(parseSentence);
+				Annotation document = new Annotation(parseSentence);
+
 				pipeline.annotate(document);
-				
+
 				List<CoreMap> sentence = document.get(SentencesAnnotation.class);
-				
+
 				int sid = 0;
-				for(CoreMap s:sentence){
+				for (CoreMap s : sentence) {
 					ParseResult pr = new ParseResult();
-					
+
 					String plainSentence = s.toShorterString("Text");
-					plainSentence = plainSentence.substring(plainSentence.indexOf("=")+1, plainSentence.length()-1);
-					//System.out.println(plainSentence);
+					plainSentence = plainSentence.substring(plainSentence.indexOf("=") + 1, plainSentence.length() - 1);
+					// System.out.println(plainSentence);
 					pr.sentence = plainSentence;
 					splittedSentence.add(plainSentence);
 
-					List<CoreLabel> tokens= s.get(TokensAnnotation.class);
-						
-					for(CoreLabel token:tokens){
-						//String word = token.word().replace("``", "\"").replace("''", "\"").replace("`", "'").replace("-LRB-", "(").replace(" ", " ")
-						//		.replace("-RRB-", ")").replace("-LSB-", "[").replace("-RSB-", "]").replace("-LCB-", "{").replace("-RCB-", "}").replace("...", ". . .");
-						//sb.append(word+"/"+token.tag()+" ");
-						//System.out.print(token.word()+"/"+token.originalText()+" "+token.tag()+" ");
+					List<CoreLabel> tokens = s.get(TokensAnnotation.class);
+
+					for (CoreLabel token : tokens) {
+						// String word = token.word().replace("``",
+						// "\"").replace("''", "\"").replace("`",
+						// "'").replace("-LRB-", "(").replace(" ", " ")
+						// .replace("-RRB-", ")").replace("-LSB-",
+						// "[").replace("-RSB-", "]").replace("-LCB-",
+						// "{").replace("-RCB-", "}").replace("...", ". . .");
+						// sb.append(word+"/"+token.tag()+" ");
+						// System.out.print(token.word()+"/"+token.originalText()+"
+						// "+token.tag()+" ");
 						pr.words.add(token.word());
 					}
 					pr.id = this.parseResults.size();
 					this.parseResults.add(pr);
-					this.sentenceMap.put(pid+"."+sid, plainSentence);
+					this.sentenceMap.put(pid + "." + sid, plainSentence);
 					sid++;
 				}
-				
-				
-				if(id==parseSentences.size()-1){
-					this.paragraphMap.put(String.valueOf(pid),splittedSentence);
-    			}
-	    	}
-	    }
+
+				if (id == parseSentences.size() - 1) {
+					this.paragraphMap.put(String.valueOf(pid), splittedSentence);
+				}
+			}
+		}
 	}
-	
+
 	public static void main(String args[]) {
 		File folder = new File("/users/ljwinnie/Desktop/bug_report/data/eclipse");
 		String[] filelist = folder.list();
-		for(int i = 0;i<2;i++){
-			String file = "/users/ljwinnie/Desktop/bug_report/data/eclipse/"+filelist[i];
+		for (int i = 0; i < 2; i++) {
+			String file = "/users/ljwinnie/Desktop/bug_report/data/eclipse/" + filelist[i];
 			System.out.println(file);
-			BugReport report = new BugReport(file,"eclipse");
+			BugReport report = new BugReport(file, "eclipse");
 		}
 	}
 }
